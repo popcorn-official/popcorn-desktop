ef3ba5aa39349e6284e87a6cd960fbc02dc4e230
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-02-20 23:01:05 -0300

    You can drag the window by the video when watching a movie

--- js/frontend/app.js
+++ js/frontend/app.js
@@ -62,6 +62,9 @@ window.spawnCallback = function (url, subs) {
     // Move this to a separate view.
     $('#video-container').html(player).show();
 
+    // Make sure you can drag the window by the video
+    $('#video-container video').canDragWindow();
+
     // Init video.
     var video = videojs('video_player');
 
@@ -160,3 +163,37 @@ jQuery(function ($) {
       }
   });
 });
+
+// Drag the window by a specific element
+(function( $ ){
+
+  $.fn.canDragWindow = function() {
+
+    return this.each(function(ix, element){
+
+      // Since the -drag CSS property fucks up the touch events, this is a hack so we can drag the window by the video anyway.
+      var mouseIsDown = false;
+      var previousPos = {};
+
+      // TODO: This breaks under multiple screens on Windows (it won't go outside the screen it's on)
+      $(element).mousedown(function(event){
+        mouseIsDown = true;
+        previousPos = {x: event.screenX, y: event.screenY};
+      }).mouseup(function(event){
+        mouseIsDown = false;
+      }).mousemove(function(event){
+
+        var thisPos = {x: event.screenX, y: event.screenY};
+        var distance = {x: thisPos.x - previousPos.x, y: thisPos.y - previousPos.y};
+        previousPos = thisPos;
+
+        if( mouseIsDown && ! win.isKioskMode ){
+          window.moveBy(distance.x, distance.y);
+        }
+      });
+
+    });
+
+  };
+
+})( jQuery );
