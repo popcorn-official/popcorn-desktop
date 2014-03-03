a1ee9da32a58827eabc6c52d4313934460728645
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-03-02 23:55:39 -0300

    Revert "Subtitles seem to work under Windows now. Also added wget because I'm lazy."
    This reverts commit 29653bde93a9d420a6740b772214fbe396fc1276.

--- js/frontend/views/sidebar.js
+++ js/frontend/views/sidebar.js
@@ -39,52 +39,57 @@ App.View.Sidebar = Backbone.View.extend({
             subtitle;
 
         function getSubtitle ( subUrl, subOutputFile ) {
-
+            var request = require('request');
             var fs = require('fs');
-            var wget = require('wget');
-            var zlib = require('zlib');
-            
+            var AdmZip = require('adm-zip');
+            var http = require('http');
+            var url = require('url');
             var charsetDetect = require('jschardet');
             var targetCharset = 'UTF-8';
             var targetEncodingCharset = 'utf8';
 
-            // Download the file (it's gzipped)
-            // Todo: This is a bug in the making. If files come in a slightly different format, this script will fail miserably.
-            var downloadSub = wget.download(subUrl, subOutputFile +'.gz' );
-            downloadSub.on('end', function(outputPath) {
-                
-                // Unzip with Zlib. adm-zip and gizp-js fuck up under Windows
-                var inp2 = fs.createReadStream(subOutputFile +'.gz');
-                var out2 = fs.createWriteStream(subOutputFile);
-                inp2.pipe(zlib.createGunzip()).pipe(out2);
-                
-                // Finished unzipping, process the final file
-                out2.on('finish', function(){
-                    var decompressedData = fs.readFileSync(subOutputFile, 'binary');
-                    var charset = charsetDetect.detect(decompressedData);
-                    
-                    if( charset.encoding == 'ascii' ){ return; } // ASCII is pretty much UTF-8
+            var options = {
+                host: url.parse(subUrl).host,
+                port: 80,
+                path: url.parse(subUrl).pathname
+            };
 
-                    var iconv = require('iconv-lite');
-                    
-                    if( charset.encoding != targetCharset && charset.encoding != targetEncodingCharset && iconv.encodingExists(charset.encoding) ) {
-                        // Windows-1251/2 works fine when read from a file (like it's UTF-8), but if you try to convert it you'll ruin the encoding.
-                        // Just save it again, and it'll be stored as UTF-8. At least on Windows.
-                        if( charset.encoding != 'windows-1251' && charset.encoding != 'windows-1252' ) {
-                            subText = iconv.encode( iconv.decode(decompressedData, charset.encoding), targetCharset );
+            http.get(options, function(res) {
+                var data = [], dataLen = 0;
+                res.on('data', function(chunk) {
+                    data.push(chunk);
+                    dataLen += chunk.length;
+                }).on('end', function() {
+                        var buf = new Buffer(dataLen);
+
+                        for (var i=0, len = data.length, pos = 0; i < len; i++) {
+                            data[i].copy(buf, pos);
+                            pos += data[i].length;
                         }
-                        fs.writeFile( subOutputFile, decompressedData);
-                    }                
-                });
-                
+                        var zip = new AdmZip(buf);
+                        var zipEntries = zip.getEntries();
+                        zipEntries.forEach(function(zipEntry, key) {
+                            if (zipEntry.entryName.indexOf('.srt') != -1) {
+                                var decompressedData = zip.readFile(zipEntry); // decompressed buffer of the entry
+                                var charset = charsetDetect.detect(decompressedData);
+                                if (charset == targetEncodingCharset || charset == targetCharset) {
+                                    fs.writeFile( subOutputFile, decompressedData);
+                                    return;
+                                } else {
+                                    var iconv = require('iconv-lite');
+                                    decompressedData = iconv.encode( iconv.decode(decompressedData, charset.encoding), targetEncodingCharset);
+                                    fs.writeFile( subOutputFile, decompressedData);
+                                    return;
+                                }
+                            }
+                        });
+                    });
             });
         }
 
         if (subs) {
             // Download all the subs so they are available during the video playback
             for( lang in subs ) {
-                // Subtitles go in the local TMP folder, as VideoJS has some trouble loading subtitles due to CORS, and even disabling security on chromium doesn't help.
-                // This affects the build process as well, as zipping everything into a package not only doesn't work, it's very slow as well.
                 subsFiles[lang] = 'tmp/' + this.model.get('title').replace(/([^a-zA-Z0-9-_])/g, '_') + '-' + this.model.get('quality') + '-' + lang + '.srt';
                 getSubtitle(subs[lang], subsFiles[lang]);
             }
