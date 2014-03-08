b733a57ff51e1d0da0addb9d38c8275291dc2d71
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-03-08 13:18:40 -0300

    Change slug formar

--- js/frontend/models/movie.js
+++ js/frontend/models/movie.js
@@ -10,7 +10,7 @@ App.Model.Movie = Backbone.Model.extend({
       model.set('bigImage', model.get('coverImage'));
       model.set('backdrop', model.get('backdropImage'));
       model.set('title',    model.get('title'));
-      model.set('slug',     (model.get('title').toLowerCase().replace(/ /g, '_') +'.'+model.get('imdb').slice(2)) );
+      model.set('slug',     model.get('imdb').slice(2) + '-' + model.get('title').toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-'));
       model.set('synopsis', model.get('synopsis'));
       model.set('voteAverage', model.get('voteAverage'));
       model.set('runtime', model.get('runtime'));
