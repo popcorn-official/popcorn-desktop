abf7e1233737583693b3a0e8c7aec8fa2150f940
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-03-13 09:41:26 +0800

    let the js know Simplified Chinese is ready

--- js/language.js
+++ js/language.js
@@ -2,7 +2,8 @@
 i18n.configure({
     defaultLocale: 'en',
     locales: ['ar', 'ca', 'da', 'de', 'en', 'es', 'eu', 'fr', 'gr', 'he', 'it', 'ja', 'kr',
-              'lt', 'nl', 'no', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'sv', 'tr', 'zh-tw'],
+              'lt', 'nl', 'no', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'sv', 'tr', 'zh-tw',
+              'zh-cn'],
     directory: './language'
 });
 
