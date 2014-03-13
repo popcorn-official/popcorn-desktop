69721d1f5205f6c86acd35d6137e4c99031774d5
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-03-13 14:50:57 +0200

    Latvian added

--- js/language.js
+++ js/language.js
@@ -2,7 +2,7 @@
 i18n.configure({
     defaultLocale: 'en',
     locales: ['ar', 'bg', 'ca', 'da', 'de', 'el', 'en', 'es', 'eu', 'fr', 'he', 'hu', 'it', 'ja', 'kr',
-              'lt', 'nl', 'no', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'sv', 'tr', 'uk', 'zh-cn', 'zh-tw'],
+              'lt', 'lv', 'nl', 'no', 'pl', 'pt', 'pt-br', 'ro', 'ru', 'sk', 'sv', 'tr', 'uk', 'zh-cn', 'zh-tw'],
     directory: './language'
 });
 
