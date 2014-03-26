0da473a20106c119fa4f3afea2fb0c05d6848aad
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-03-26 17:06:06 -0400

    Add movie list

--- src/app/lib/views/movie_browser/movie_browser.js
+++ src/app/lib/views/movie_browser/movie_browser.js
@@ -1,6 +1,14 @@
 (function(App) {
     "use strict";
 
+    /**
+     * Manage movie browsing:
+     *  * Create filter views
+     *  * Create movie list
+     *  * Fetch new movie collection and pass them to the movie list view
+     *  * Show movie detail
+     *  * Start playing a movie
+     */
     var MovieBrowser = Backbone.View.extend({
         className: 'movie-browser',
 
@@ -8,6 +16,17 @@
             this.categoryList = new App.View.CategoryList({
                 el: this.$('.category-list'),
                 model: App.Config.categories
+            });
+
+            this.movieCollection = new App.Config.Provider.Movies();
+            this.movieList = new App.View.MovieList({
+                el: this.$('.movie-list'),
+                model: this.movieCollection
+            });
+
+            // Fetch default category movie:
+            this.movieCollection.fetch({
+                category: App.Config.categories[0]
             });
         },
 
---
+++ src/app/lib/views/movie_browser/movie_list.js
@@ -0,0 +1,14 @@
+(function(App) {
+    "use strict";
+
+    var MovieList = Backbone.View.extend({
+        render: function() {
+        },
+
+        load: function(filter) {
+
+        }
+    });
+
+    App.View.MovieList = MovieList;
+})(window.App);
