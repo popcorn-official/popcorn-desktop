0c1a96f7a7826d99f3118b8ff315e87d5c94c581
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-04-25 22:30:46 +0100

    Get rid of console log

--- src/app/lib/views/show_browser/show_item.js
+++ src/app/lib/views/show_browser/show_item.js
@@ -35,7 +35,6 @@
             App.db.getTVShow({show_id: this.model.get("_id")}, function(err, data) {
                 // we send our DB data to our view
                 App.vent.trigger('show:showDetail', new Backbone.Model(data[0]));
-                console.log(data[0]);
             });
             
         }
