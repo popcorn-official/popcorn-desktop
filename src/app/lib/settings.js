e6e309e3581036d255d5d76b8fd4aa83835dd335
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-04-28 13:02:11 -0400

    Add default value to settings
    To get DB settings works properly we need to set default value

--- src/app/lib/settings.js
+++ src/app/lib/settings.js
@@ -3,6 +3,12 @@
 
     var Settings = [];
 
+    // ATTENTION
+    // TO get settings works properly we need to set default value
+
+    Settings['language'] = 'en';
+    Settings['font'] = 'tahoma';
+
     App.db.getSettings(function(err, data) {
         if (data != null) {
             for(var key in data) {
