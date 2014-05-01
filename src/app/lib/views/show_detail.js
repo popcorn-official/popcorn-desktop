9a059cc05dfef4ccb644e46bb5fb755d94cc6fc8
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-05-01 21:33:24 +0100

    Update show_detail.js

--- src/app/lib/views/show_detail.js
+++ src/app/lib/views/show_detail.js
@@ -13,7 +13,8 @@
         events: {
             'click .startStreaming': 'startStreaming',
             'click .tv-container-close': 'closeDetails',
-            'click #tabs_season li.seasonSwitch a': 'clickTab'
+            'click #tabs_season li': 'clickTab',
+            'click .EpisodeData': 'clickEpisode',
         },
 
         onShow: function() {
@@ -51,7 +52,26 @@
                 $(e.currentTarget).parent().attr("id","current_season"); // Activate this
                 $( $(e.currentTarget).attr('href')).fadeIn(); // Show tabs_container for current tab
             }
+            var tab_id = $(this).attr('data-tab');
+
+            $('#tabs_season li').removeClass('active');
+            $('.tabs-episode').removeClass('current');
+            $('.epidoseSummary').removeClass('active');
+            $(this).addClass('active');
+            $("#"+tab_id).addClass('current');
         },
+
+         clickEpisode: function(e) {
+            e.preventDefault();
+            
+            $('.epidoseSummary').removeClass('active');
+            
+            $(this).parent().addClass('active');
+            $(".episode-info p").text('Episode '+$(this).attr('data-id'));
+            $(".episode-info div").text('Resume of Episode '+$(this).children().val());
+
+         },
+
 
 
 
