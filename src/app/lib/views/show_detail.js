2a2ed51e985c136f7856b0da8126e32fbfad2689
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-05-06 13:27:10 +0100

    Update show_detail.js

--- src/app/lib/views/show_detail.js
+++ src/app/lib/views/show_detail.js
@@ -101,7 +101,7 @@
             var tvdbtorrent = $elem.attr('data-torrent');
             $('.epidoseSummary').removeClass('active');
             $elem.parent().addClass('active');
-            $(".episode-info-number").text($('.template-'+tvdbid+' .episode').html());
+            $(".episode-info-number").text('Episode '+$('.template-'+tvdbid+' .episode').html());
             $(".episode-info-title").text($('.template-'+tvdbid+' .title').html());
             $(".episode-info-date").text($('.template-'+tvdbid+' .date').html());
             $(".episode-info-description").text($('.template-'+tvdbid+' .overview').html());
