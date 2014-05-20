e44ee70ee8097e786e9722c16583ae5d979c87ec
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-05-20 20:05:58 -0300

    use existing loading

--- src/app/lib/views/show_browser/show_item.js
+++ src/app/lib/views/show_browser/show_item.js
@@ -10,7 +10,6 @@
         ui: {
             coverImage: '.cover-image',
             cover: '.cover',
-            coverOverlay: '.cover-overlay',
             bookmarkIcon: '.actions-favorites'
         },
 
@@ -52,21 +51,13 @@
 
         // triggered on click only
         showDetail: function() {
-            var that = this;
-            if (!this.blocked) {
-                this.ui.bookmarkIcon.hide();
-                this.blocked = true;
-                this.ui.coverOverlay.prepend('<div id="element-loading"><div id="loading-more-animi" class="loading-container"><div class="ball"></div><div class="ball1"></div></div></div>');
-                var tvshow = new (App.Config.getProvider('tvshow'))();
-                var data = tvshow.detail(this.model.get('imdb_id'), function(err, data) {
-                    that.blocked = false;
-                    that.ui.bookmarkIcon.show();
-                    that.ui.coverOverlay.find('#element-loading').remove();
-                    if (!err) App.vent.trigger('show:showDetail', new Backbone.Model(data));
-                    else alert("Somethings wrong... try later");
-                });
-            }
-
+            $('.spinner').show();
+            var tvshow = new (App.Config.getProvider('tvshow'))();
+            var data = tvshow.detail(this.model.get('imdb_id'), function(err, data) {
+                $('.spinner').hide();
+                if (!err) App.vent.trigger('show:showDetail', new Backbone.Model(data));
+                else alert("Somethings wrong... try later");
+            });
         },
 
         toggleFavorite: function(e) {
