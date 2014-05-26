1d136f80d71b25bd457f62f2b036b7a37e84cfb9
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-05-26 02:37:03 -0300

    Reorder settings. 1000 DHT limit is enough for default.

--- src/app/settings.js
+++ src/app/settings.js
@@ -3,35 +3,39 @@
     os = require('os'),
     path = require('path');
     
-    // default settings
-    Settings.updateApiEndpoint = 'http://get-popcorn.com/';
-    
-    // TODO: Buy SSL for main domain + buy domain get-popcorn.re for fallback
-    //Settings['updateApiEndpointMirror'] = 'https://get-popcorn.re/';
+    /** Default settings **/
 
-    Settings.yifyApiEndpoint = 'https://yts.re/api/';
-    Settings.yifyApiEndpointMirror = 'https://yts.im/api/';
-
-    // default tvshow api endpoint
-    Settings.tvshowApiEndpoint = 'http://eztvapi.re/';
-
-    Settings.connectionCheckUrl = 'http://google.com/';
-    Settings.moviesShowQuality = false;
+    // User interface
+	Settings.language = 'en';
 	
+    // Movies
+    Settings.moviesShowQuality = false;
+    Settings.movies_quality = 'all';
+	
+    // Subtitles
+    Settings.subtitle_language = 'none';
     Settings.subtitle_size = '28px';
 	
-    Settings.movies_quality = 'all';
+    // More options
+    Settings.tvshowApiEndpoint = 'http://eztvapi.re/';
 
+    // Advanced options
     Settings.connectionLimit = 100;
-    Settings.dhtLimit = 10000;
+    Settings.dhtLimit = 1000;
     Settings.tmpLocation = path.join(os.tmpDir(), 'Popcorn-Time');
     Settings.deleteTmpOnClose = true;
-
-    // app Settings
+	
+    // Hidden endpoints
+    Settings.updateApiEndpoint = 'http://get-popcorn.com/';
+    /* TODO: Buy SSL for main domain + buy domain get-popcorn.re for fallback
+    Settings.updateApiEndpointMirror = 'https://get-popcorn.re/'; */
+    Settings.yifyApiEndpoint = 'https://yts.re/api/';
+    Settings.yifyApiEndpointMirror = 'https://yts.im/api/';
+    Settings.connectionCheckUrl = 'http://google.com/';
+	
+    // App Settings
     Settings.version = false;
     Settings.dbversion = '0.1.0';
-    Settings.language = false;
-    Settings.subtitle_language = 'none'; // none by default
     Settings.font = 'tahoma';
 
     var AdvSettings = {
