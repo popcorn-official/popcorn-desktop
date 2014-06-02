76cbfa02082193c8ca8941e9bd409595fe7379df
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-06-02 05:20:04 -0300

    Redone movie details code

--- src/app/lib/views/movie_detail.js
+++ src/app/lib/views/movie_detail.js
@@ -1,243 +1,224 @@
 (function(App) {
-    'use strict';
+	'use strict';
 
-    App.View.MovieDetail = Backbone.Marionette.ItemView.extend({
-        template: '#movie-detail-tpl',
-        className: 'app-overlay',
+	App.View.MovieDetail = Backbone.Marionette.ItemView.extend({
+		template: '#movie-detail-tpl',
+		className: 'app-overlay',
 
-        ui: {
-            selected_lang: '.selected-lang',
-            bookmarkIcon: '.cover-detail-favorites'
-        },
+		ui: {
+			selected_lang: '.selected-lang',
+			bookmarkIcon: '.detail-favourites'
+		},
 
-        events: {
-            'click .movie-btn-watch': 'startStreaming',
-            'click .movie-btn-watch-trailer': 'playTrailer',
-            'click .movie-detail-close': 'closeDetails',
-            'click #switch-hd-on': 'enableHD',
-            'click #switch-hd-off': 'disableHD',
-            'click #toggle-sub-dropdown': 'toggleDropdown',
-            'click .sub-dropdown-arrow-down': 'toggleDropdown',
-            'click .sub-flag-icon': 'closeDropdown',
-            'click .sub-dropdown-arrow-up': 'closeDropdown',
-            'click .cover-detail-favorites': 'toggleFavorite',
-            'click .movie-imdb-link': 'openIMDb'
-        },
+		events: {
+			'click .movie-btn.watch': 'startStreaming',
+			'click .movie-btn.trailer': 'playTrailer',
+			'click .movie-detail-close': 'closeDetails',
+			'click #switch-hd-on': 'enableHD',
+			'click #switch-hd-off': 'disableHD',
+			'click .sub-dropdown': 'toggleDropdown',
+			'click .sub-flag-icon': 'closeDropdown',
+			'click .detail-favourites': 'toggleFavourite',
+			'click .movie-imdb-link': 'openIMDb'
+		},
 
-        onShow: function() {
-        
+		onShow: function() {
+			win.info('Show movie detail');
 
-            var that = this;
-            Mousetrap.bind('esc', function(e) {
-                that.closeDetails();
-            });
+			var torrents = this.model.get('torrents');
+			if (torrents['720p'] !== undefined && torrents['1080p'] !== undefined) {
+				this.model.set('quality', torrents['1080p'].url);
+				this.calcHealth(torrents['1080p']);
+			} else if(torrents['1080p'] !== undefined ) {
+				this.model.set('quality', torrents['1080p'].url);
+				this.calcHealth(torrents['1080p']);
+			} else if(torrents['720p'] !== undefined ) {
+				this.model.set('quality', torrents['720p'].url);
+				this.calcHealth(torrents['720p']);
+			} else if(torrents['HDRip'] !== undefined ) {
+				this.model.set('quality', torrents['HDRip'].url);
+				this.calcHealth(torrents['HDRip']);
+			}
 
-            // TODO: use the default in settings
-            this.subtitle_selected = false;
+			$('.star-container,.movie-imdb-link').tooltip();
 
-            win.info('Show movie detail');
+			var background = $('.movie-backdrop').attr('data-bgr');
+			$('<img/>').attr('src', background).on('load', function() {
+				$(this).remove();
+				$('.movie-backdrop').css('background-image', 'url(' + background + ')').fadeIn(1000);
+			});
+			
+			var coverurl = $('.movie-cover-image').attr('data-cover');
+			$('<img/>').attr('src', coverurl).on('load', function() {
+				$(this).remove();
+				$('.movie-cover-image').attr('src', coverurl).fadeTo(1000, 1);
+			});
 
-            var torrents = this.model.get('torrents');
-            if (torrents['720p'] !== undefined && torrents['1080p'] !== undefined) {
-                this.model.set('quality', torrents['1080p'].url);
-                this.calcHealth(torrents['1080p']);
-            } else if(torrents['1080p'] !== undefined ) {
-                this.model.set('quality', torrents['1080p'].url);
-                this.calcHealth(torrents['1080p']);
-            } else if(torrents['720p'] !== undefined ) {
-                this.model.set('quality', torrents['720p'].url);
-                this.calcHealth(torrents['720p']);
-            } else if(torrents['HDRip'] !== undefined ) {
-                this.model.set('quality', torrents['HDRip'].url);
-                this.calcHealth(torrents['HDRip']);
-            }
+			// switch to default subtitle
+			this.switchSubtitle(Settings.subtitle_language);
+			
+			if (this.model.get('bookmarked') === true) {
+				this.ui.bookmarkIcon.addClass('selected').text( i18n.__('Remove from bookmarks') );
+			}
 
-            $('.star-container,.movie-imdb-link').tooltip();
+			// add ESC to close this popup
+			Mousetrap.bind('esc', function(e) {
+				App.vent.trigger('movie:closeDetail');
+			});
 
-            var background = $('.movie-backdrop').attr('data-bgr');
+		},
 
-            $('<img/>').attr('src', background).load(function() {
-                $(this).remove();
-                $('.movie-backdrop').css('background-image', 'url(' + background + ')').fadeIn(300);
-            });
+		onClose: function() {},
+		
+		toggleDropdown: function(e) {
+			if($('.sub-dropdown').is('.open')) {
+				this.closeDropdown(e);
+				return false;
+			}else{
+				$('.sub-dropdown').addClass('open');
+			}
+			var self = this;
+			$('.flag-container').fadeIn();
+		},
 
-            $('.sub-dropdown-arrow-down').show();
+		closeDropdown: function(e) {
+			e.preventDefault();
+			$('.flag-container').fadeOut();
+			$('.sub-dropdown').removeClass('open');
+			
+			var value = $(e.currentTarget).attr('data-lang');
+			if(value) {
+				this.switchSubtitle(value);
+			}
+		},
+		
+		switchSubtitle: function(lang) {
+			var subtitles = this.model.get('subtitle');
 
-            // switch to default subtitle
-            this.switchSubtitle(Settings.subtitle_language);
+			if (subtitles === undefined || subtitles[lang] === undefined) {
+				lang = 'none';
+			}
 
-            // add ESC to close this popup
-            Mousetrap.bind('esc', function(e) {
-                App.vent.trigger('movie:closeDetail');
-            });
+			this.subtitle_selected = lang;
+			this.ui.selected_lang.removeClass().addClass('flag toggle selected-lang').addClass(this.subtitle_selected);
 
-            $(window).on('resize', this.onThisResize);
-            this.onThisResize();
+			win.info('Subtitle: ' + this.subtitle_selected);
+		},
 
-        },
+		startStreaming: function() {
+			var torrentStart = new Backbone.Model({
+				torrent: this.model.get('quality'),
+				backdrop: this.model.get('backdrop'),
+				subtitle: this.model.get('subtitle'),
+				defaultSubtitle: this.subtitle_selected,
+				title: this.model.get('title'),
+				type: 'movie',
+				imdb_id: this.model.get('imdb_id')
+			});
+			App.vent.trigger('stream:start', torrentStart);
+		},
 
-        onThisResize: function() {
- 
-            $('.cover-detail-overlay').height( $('.movie-cover-image').height() );
-        },
+		playTrailer: function() {
+			var trailer = new Backbone.Model({
+				src: this.model.get('trailer'), 
+				type: 'video/youtube', 
+				subtitle: null, 
+				title: this.model.get('title') 
+			});
+			App.vent.trigger('stream:ready', trailer);
+		},
 
-        onClose: function() {
-        },
-        showCover: function() {},
-        toggleDropdown: function() {
-            var self = this;
-            $('.flag-container').fadeIn();
-            $('.sub-dropdown-arrow-down').hide();
-            $('.sub-dropdown-arrow-up').show();
-            $('#toggle-sub-dropdown').one('click', function(e) {
-                self.closeDropdown(e);
-                return false;
-            });
+		closeDetails: function() {
+			App.vent.trigger('movie:closeDetail');
+		},
 
-        },
+		enableHD: function () {
+			var torrents = this.model.get('torrents');
+			win.info('HD Enabled');
 
-        closeDropdown: function(e) {
-            e.preventDefault();
-            var value = ($(e.currentTarget).attr('data-lang') == null) ? 'none' : $(e.currentTarget).attr('data-lang');
-            this.switchSubtitle(value);
-        },
+			if (torrents['1080p'] !== undefined) {
+				torrents = this.model.get('torrents');
+				this.model.set('quality', torrents['1080p'].url);
+				this.calcHealth(torrents['1080p']);
+				win.debug(this.model.get('quality'));
+			}
+		},
 
-        startStreaming: function() {
-            var torrentStart = new Backbone.Model({
-                torrent: this.model.get('quality'), 
-                backdrop: this.model.get('backdrop'), 
-                subtitle: this.model.get('subtitle'), 
-                defaultSubtitle: this.subtitle_selected, 
-                title: this.model.get('title'),
-                type: 'movie',
-                imdb_id: this.model.get('imdb_id')
-            });
-            App.vent.trigger('stream:start', torrentStart);
-        },
+		disableHD: function () {
+			var torrents = this.model.get('torrents');
+			win.info('HD Disabled');
 
-        playTrailer: function() {
-            var trailer = new Backbone.Model({
-                src: this.model.get('trailer'), 
-                type: 'video/youtube', 
-                subtitle: null, 
-                title: this.model.get('title') 
-            });
-            App.vent.trigger('stream:ready', trailer);
-        },
+			if (torrents['720p'] !== undefined) {
+				torrents = this.model.get('torrents');
+				this.model.set('quality', torrents['720p'].url);
+				this.calcHealth(torrents['720p']);
+				win.debug(this.model.get('quality'));
+			}
+		},
 
-        closeDetails: function() {
-            App.vent.trigger('movie:closeDetail');
-        },
-
-        enableHD: function () {
-            var torrents = this.model.get('torrents');
-            win.info('HD Enabled');
-
-            if (torrents['1080p'] !== undefined) {
-                torrents = this.model.get('torrents');
-                this.model.set('quality', torrents['1080p'].url);
-                this.calcHealth(torrents['1080p']);
-                win.debug(this.model.get('quality'));
-            }
-        },
-
-        disableHD: function () {
-            var torrents = this.model.get('torrents');
-            win.info('HD Disabled');
-
-            if (torrents['720p'] !== undefined) {
-                torrents = this.model.get('torrents');
-                this.model.set('quality', torrents['720p'].url);
-                this.calcHealth(torrents['720p']);
-                win.debug(this.model.get('quality'));
-            }
-        },
-
-        calcHealth: function (tQ) {
-            var spratio = tQ.seed / tQ.peer;
-            var health = 'Bad';
-            if(spratio > 5){
-                health = tQ.seed > 100? 'Excellent':'Good';
-            }else if(spratio > 2){
-                health = tQ.seed > 100? 'Good':'Medium';
-            }else if(spratio >= 1){
-                health = tQ.seed > 100? 'Medium':'Bad';
-            }
-            $('.health-icon').tooltip({html: true})
-                             .removeClass('Bad Medium Good Excellent')
-                             .addClass(health)
-                             .attr('data-original-title', i18n.__('Health ' + health) + ' - ' + i18n.__('Ratio') + ': ' + spratio.toFixed(2) + ' <br> ' + i18n.__('Seeds') + ': ' + tQ.seed + ' - ' + i18n.__('Peers') + ': ' + tQ.peer)
-                             .tooltip('fixTitle');
-        },
-
-
-        toggleFavorite: function(e) {
-            e.stopPropagation();
-            e.preventDefault();
-            var that = this;
-            if (this.model.get('bookmarked') === true) {
-                Database.deleteBookmark(this.model.get('imdb'), function(err, data) {
-                    console.log('Bookmark deleted');
-                    that.model.set('bookmarked', false);
-                    that.ui.bookmarkIcon.removeClass('selected');
-                    // we'll make sure we dont have a cached movie
-                    Database.deleteMovie(that.model.get('imdb'),function(err, data) {});
-                });
-            } else {
-
-                // we need to have this movie cached
-                // for bookmarking
-                var movie = {
-                    imdb: this.model.get('imdb'),
-                    image: this.model.get('image'),
-                    torrents: this.model.get('torrents'),
-                    title: this.model.get('title'),
-                    synopsis: this.model.get('synopsis'),
-                    runtime: this.model.get('runtime'),
-                    year: this.model.get('year'),
-                    health: this.model.get('health'),
-                    subtitle: this.model.get('subtitle'),
-                    backdrop: this.model.get('backdrop'),
-                    rating: this.model.get('MovieRating'),
-                    trailer: this.model.get('trailer'),
-                };
-
-                Database.addMovie(movie, function(error,result) {
-                    Database.addBookmark(that.model.get('imdb'), 'movie', function(err, data) {
-                        console.log('Bookmark added');
-                        that.ui.bookmarkIcon.addClass('selected');
-                        that.model.set('bookmarked', true);
-                    });
-                });
-
-            }
-        },
+		calcHealth: function (tQ) {
+			var spratio = tQ.seed / tQ.peer;
+			var health = 'Bad';
+			if(spratio > 5){
+				health = tQ.seed > 100? 'Excellent':'Good';
+			}else if(spratio > 2){
+				health = tQ.seed > 100? 'Good':'Medium';
+			}else if(spratio >= 1){
+				health = tQ.seed > 100? 'Medium':'Bad';
+			}
+			$('.health-icon').tooltip({html: true})
+							 .removeClass('Bad Medium Good Excellent')
+							 .addClass(health)
+							 .attr('data-original-title', i18n.__('Health ' + health) + ' - ' + i18n.__('Ratio') + ': ' + spratio.toFixed(2) + ' <br> ' + i18n.__('Seeds') + ': ' + tQ.seed + ' - ' + i18n.__('Peers') + ': ' + tQ.peer)
+							 .tooltip('fixTitle');
+		},
 
 
-        // helper function to switch subtitle
-        switchSubtitle: function(lang) {
+		toggleFavourite: function(e) {
+			e.stopPropagation();
+			e.preventDefault();
+			var that = this;
+			if (this.model.get('bookmarked') === true) {
+				Database.deleteBookmark(this.model.get('imdb'), function(err, data) {
+					console.log('Bookmark deleted');
+					that.model.set('bookmarked', false);
+					that.ui.bookmarkIcon.removeClass('selected').text( i18n.__('Add to bookmarks') );
+					// we'll make sure we dont have a cached movie
+					Database.deleteMovie(that.model.get('imdb'),function(err, data) {});
+				});
+			} else {
 
-            // did this lang exist ?
-            var subtitles = this.model.get('subtitle');
+				// we need to have this movie cached
+				// for bookmarking
+				var movie = {
+					imdb: this.model.get('imdb'),
+					image: this.model.get('image'),
+					torrents: this.model.get('torrents'),
+					title: this.model.get('title'),
+					synopsis: this.model.get('synopsis'),
+					runtime: this.model.get('runtime'),
+					year: this.model.get('year'),
+					health: this.model.get('health'),
+					subtitle: this.model.get('subtitle'),
+					backdrop: this.model.get('backdrop'),
+					rating: this.model.get('MovieRating'),
+					trailer: this.model.get('trailer'),
+				};
 
-            // make sure we have an existing lang
-            if (subtitles === undefined || subtitles[lang] === undefined) {
-                lang = 'none';
-            }
+				Database.addMovie(movie, function(error,result) {
+					Database.addBookmark(that.model.get('imdb'), 'movie', function(err, data) {
+						console.log('Bookmark added');
+						that.ui.bookmarkIcon.addClass('selected').text( i18n.__('Remove from bookmarks') );
+						that.model.set('bookmarked', true);
+					});
+				});
 
-            // here we go...
-            this.subtitle_selected = lang;
-            this.ui.selected_lang.removeClass().addClass('flag').addClass('toggle').addClass('selected-lang').addClass(this.subtitle_selected);
-            $('.flag-container').fadeOut();
-            $('.sub-dropdown-arrow-down').show();
-            $('.sub-dropdown-arrow-up').hide();
+			}
+		},
 
-            win.info('Subtitle: ' + this.subtitle_selected);
-        },
+		openIMDb: function() {
+			gui.Shell.openExternal('http://www.imdb.com/title/' + this.model.get('imdb_id'));
+		}
 
-        openIMDb: function() {
-            gui.Shell.openExternal('http://www.imdb.com/title/' + this.model.get('imdb_id'));
-        }
-
-    });
+	});
 })(window.App);
--- src/app/templates/movie-detail.tpl
+++ src/app/templates/movie-detail.tpl
@@ -1,75 +1,84 @@
+<%  
+if(typeof backdrop === "undefined"){ backdrop = ""; }; 
+if(typeof synopsis === "undefined"){ synopsis = "Synopsis not available."; }; 
+if(typeof runtime === "undefined"){ runtime = "N/A"; }; 
+%>
 <div class="movie-detail">
-	<div class="detail-window">
-	<%  
-		if(typeof backdrop === 'undefined'){ backdrop = ''; }; 
-		if(typeof synopsis === 'undefined'){ synopsis = 'Synopsis not available.'; }; 
- 		if(typeof runtime === 'undefined'){ runtime = 'N/A'; }; 
- 	%>
-		<div id="movie-backdrop" data-bgr="<%= backdrop %>" class="movie-backdrop"></div>
+	<div id="movie-backdrop" data-bgr="<%= backdrop %>" class="movie-backdrop">
 		<div class="movie-backdrop-overlay"></div>
-		<div class="cover-detail-overlay"><div class="cover-detail-favorites"></div></div>
-		<img class="movie-cover-image" src="<%= image %>" />
-		<div class="sperator-movie"></div>
-		<div class="movie-meta-title"><%= title %></div>
-		<div class="movie-quick-info">
-			<div class="movie-meta-details"><%= year %></div>
-			<div class="movie-dot"></div>
-			<% if (synopsis.length > 528) { var synopsis = synopsis.substring(0, 528) + "..."; } %>
-			<div class="movie-meta-details"><%= runtime %> min</div>
-			<div class="movie-dot"></div>
-			<div data-toggle="tooltip" data-placement="top" title="<%=i18n.__("Open IMDb page") %>" class="movie-imdb-link"></div>
-			<div class="movie-dot"></div>
-			<% var p_rating = Math.round(rating) / 2; %>
-			<div data-toggle="tooltip" data-placement="right" title="<%= rating %>/10" class="star-container">
-				<% for (var i = 1; i <= Math.floor(p_rating); i++) { %>
-					<div class="rating-star full"></div>
-				<% }; %>
-				<% if (p_rating % 1 > 0) { %>
-					<div class="rating-star half"></div>
-				<% }; %>
-				<% for (var i = Math.ceil(p_rating); i < 5; i++) { %>
-					<div class="rating-star null"></div>
-				<% }; %>
+	</div>
+	<div class="detail-window">
+		<div class="movie-detail-close"></div>
+		<img class="movie-cover-image" src="images/cover-placeholder.jpg" data-cover="<%= bigImage %>" />
+		<div class="movie-data">
+			<div class="movie-meta-title"><%= title %></div>
+			<div class="movie-quick-info">
+				<div class="movie-meta-details"><%= year %></div>
+				<div class="movie-dot"></div>
+				<% if (synopsis.length > 528) { var synopsis = synopsis.substring(0, 528) + "..."; } %>
+				<div class="movie-meta-details"><%= runtime %> min</div>
+				<div class="movie-dot"></div>
+				<div data-toggle="tooltip" data-placement="top" title="<%=i18n.__("Open IMDb page") %>" class="movie-imdb-link"></div>
+				<div class="movie-dot"></div>
+				<% var p_rating = Math.round(rating) / 2; %>
+				<div data-toggle="tooltip" data-placement="right" title="<%= rating %>/10" class="star-container">
+					<% for (var i = 1; i <= Math.floor(p_rating); i++) { %>
+						<div class="rating-star full"></div>
+					<% }; %>
+					<% if (p_rating % 1 > 0) { %>
+						<div class="rating-star half"></div>
+					<% }; %>
+					<% for (var i = Math.ceil(p_rating); i < 5; i++) { %>
+						<div class="rating-star null"></div>
+					<% }; %>
+				</div>
+				
+				<div data-toggle="tooltip" data-placement="left" title="Health <%= health %>" class="health-icon Excellent"></div>
+			</div>
+
+			<div class="movie-overview-container">
+				<div class="movie-meta-overview"><%= synopsis %></div>
 			</div>
 			
-			<div data-toggle="tooltip" data-placement="left" title="Health <%= health %>" class="health-icon Excellent"></div>
-		</div>
-
-		<div class="movie-overview-container">
-			<div class="movie-meta-overview"><%= synopsis %></div>
-		</div>
-
-		<div class="movie-detail-close"></div>
-
-		<div class="movie-controlls-container">
-
-			<div class="movie-btn-watch"><div class="movie-watch-now"><%= i18n.__("Watch Now") %></div></div>
-
-			<div class="movie-btn-watch-trailer">
-				<div class="movie-watch-now"><%= i18n.__("Watch Trailer") %></div>
-			</div>
-
-			<div class="movie-quality-container">
-				<% if (torrents["720p"] !== undefined && torrents["1080p"] !== undefined) { %>
-					<div class="q720">720</div>
-					<div class="q1080">1080</div>
-					<div class="quality switch white">
-						<input type="radio" name="switch" id="switch-hd-off" >
-						<input type="radio" name="switch" id="switch-hd-on" checked >
-						<span class="toggle"></span>
+			<div class="movie-bottom">
+				<div class="movie-buttons">
+					<div class="detail-favourites"><%=i18n.__("Add to bookmarks") %></div>
+						
+					<div class="sub-dropdown"><%= i18n.__("Subtitles") %>
+						<div class="flag toggle selected-lang none"></div>
+						<div class="sub-dropdown-arrow"></div>
 					</div>
-				<% } else { %>
-					<div class="q720"><% if (torrents["720p"] !== undefined) { %>720p<% }else if (torrents["1080p"] !== undefined) { %>1080p<% } else { %>HDRip<% } %> </div>
-				<% } %>
-			</div>
 
-			<div class="sub-dropdown"><%= i18n.__("Subtitles") %><div id="toggle-sub-dropdown" class="flag toggle NA selected-lang"></div>
-			<div id="toggle-sub-dropdown-arrow" class="sub-dropdown-arrow-down"></div> <div class="sub-dropdown-arrow-up"></div> </div>
+					<div class="flag-container">
+						<div class="sub-flag-icon flag none" data-lang="none" title="<%= i18n.__("Disabled") %>"></div>
+						<% for(var lang in subtitle){ %>
+							<div class="sub-flag-icon flag <%= lang %>" data-lang="<%= lang %>" title="<%= App.Localization.langcodes[lang].nativeName %>"></div>
+						<% } %>
+					</div>
+				</div>
 
-			<div class="flag-container">
-				<% for(var lang in subtitle){ %>
-					<div class="sub-flag-icon flag <%= lang %>" data-lang="<%= lang %>" title="<%= App.Localization.langcodes[lang].nativeName %>"></div>
-				<% } %>
+				<div class="movie-controls-container">
+
+					<div class="movie-btn watch"><div class="movie-watch-now"><%= i18n.__("Watch Now") %></div></div>
+
+					<div class="movie-btn trailer">
+						<div class="movie-watch-now"><%= i18n.__("Watch Trailer") %></div>
+					</div>
+
+					<div class="movie-quality-container">
+						<% if (torrents["720p"] !== undefined && torrents["1080p"] !== undefined) { %>
+							<div class="q720">720</div>
+							<div class="q1080">1080</div>
+							<div class="quality switch white">
+								<input type="radio" name="switch" id="switch-hd-off" >
+								<input type="radio" name="switch" id="switch-hd-on" checked >
+								<span class="toggle"></span>
+							</div>
+						<% } else { %>
+							<div class="q720"><% if (torrents["720p"] !== undefined) { %>720p<% }else if (torrents["1080p"] !== undefined) { %>1080p<% } else { %>HDRip<% } %> </div>
+						<% } %>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
