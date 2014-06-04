6bc64c5ef54182ad485bea48dfbd9db8b3f9db4a
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-06-04 18:04:30 -0300

    Fix hasMore in movies
    Fix #768

--- src/app/lib/models/movie_collection.js
+++ src/app/lib/models/movie_collection.js
@@ -1,80 +1,78 @@
 (function(App) {
-    'use strict';
+	'use strict';
 
-    var Q = require('q');
+	var Q = require('q');
 
-    var MovieCollection = Backbone.Collection.extend({
-        model: App.Model.Movie,
+	var MovieCollection = Backbone.Collection.extend({
+		model: App.Model.Movie,
 
-        initialize: function(models, options) {
-            this.providers = {
-                torrent: new (App.Config.getProvider('movie'))(),
-                subtitle: new (App.Config.getProvider('subtitle'))(),
-                metadata: new (App.Config.getProvider('metadata'))()
-            };
+		initialize: function(models, options) {
+			this.providers = {
+				torrent: new (App.Config.getProvider('movie'))(),
+				subtitle: new (App.Config.getProvider('subtitle'))(),
+				metadata: new (App.Config.getProvider('metadata'))()
+			};
 
-            options = options || {};
-            options.filter = options.filter || new App.Model.Filter();
+			options = options || {};
+			options.filter = options.filter || new App.Model.Filter();
 
-            this.filter = _.defaults(_.clone(options.filter.attributes), {page: 1});
-            this.hasMore = true;
+			this.filter = _.defaults(_.clone(options.filter.attributes), {page: 1});
+			this.hasMore = true;
 
-            Backbone.Collection.prototype.initialize.apply(this, arguments);
-        },
+			Backbone.Collection.prototype.initialize.apply(this, arguments);
+		},
 
-        fetch: function() {
-            var self = this;
+		fetch: function() {
+			var self = this;
 
-            if(this.state === 'loading' && !this.hasMore) {
-                return;
-            }
+			if(this.state === 'loading' && !this.hasMore) {
+				return;
+			}
 
-            this.state = 'loading';
-            self.trigger('loading', self);
+			this.state = 'loading';
+			self.trigger('loading', self);
 
-            var subtitle = this.providers.subtitle;
-            var metadata = this.providers.metadata;
-            var torrent = this.providers.torrent;
-            var torrentPromise = torrent.fetch(this.filter);
-            var idsPromise = torrentPromise.then(_.bind(torrent.extractIds, torrent));
-            var subtitlePromise = idsPromise.then(_.bind(subtitle.fetch, subtitle));
-            var metadataPromise = idsPromise.then(_.bind(metadata.fetch, metadata));
+			var subtitle = this.providers.subtitle;
+			var metadata = this.providers.metadata;
+			var torrent = this.providers.torrent;
+			var torrentPromise = torrent.fetch(this.filter);
+			var idsPromise = torrentPromise.then(_.bind(torrent.extractIds, torrent));
+			var subtitlePromise = idsPromise.then(_.bind(subtitle.fetch, subtitle));
+			var metadataPromise = idsPromise.then(_.bind(metadata.fetch, metadata));
+			
+			
+			return Q.all([torrentPromise, subtitlePromise, metadataPromise])
+				.spread(function(torrents, subtitles, metadatas) {
+					// If a new request was started...
+					_.each(torrents.movies, function(movie){
+						var id = movie.imdb;
+						var info = metadatas[id] || {};
+						movie.subtitle = subtitles[id];
+						_.extend(movie, _.pick(info, [
+							'image','backdrop',
+							'synopsis','genres','certification','runtime',
+							'tagline','title','trailer','year'
+						]));
+					});
 
-            return Q.all([torrentPromise, subtitlePromise, metadataPromise])
-                .spread(function(movies, subtitles, metadatas) {
-                    // If a new request was started...
-                    _.each(movies, function(movie){
-                        var id = movie.imdb;
-                        var info = metadatas[id] || {};
-                        movie.subtitle = subtitles[id];
-                        _.extend(movie, _.pick(info, [
-                            'image','backdrop',
-                            'synopsis','genres','certification','runtime',
-                            'tagline','title','trailer','year'
-                        ]));
-                    });
+					self.add(torrents.movies);
+					self.hasMore = torrents.hasMore;
+					self.trigger('sync', self);
+					self.state = 'loaded';
+					self.trigger('loaded', self, self.state);
+				})
+				.catch(function(err) {
+					self.state = 'error';
+					self.trigger('loaded', self, self.state);
+					win.error(err.message, err.stack);
+				});
+		},
 
-                    if(_.isEmpty(movies) || movies.length < 40) {
-                        self.hasMore = false;
-                    }
+		fetchMore: function() {
+			this.filter.page += 1;
+			this.fetch();
+		}
+	});
 
-                    self.add(movies);
-                    self.trigger('sync', self);
-                    self.state = 'loaded';
-                    self.trigger('loaded', self, self.state);                    
-                })
-                .catch(function(err) {
-                    self.state = 'error';
-                    self.trigger('loaded', self, self.state);
-                    win.error(err.message, err.stack);
-                });
-        },
-
-        fetchMore: function() {
-            this.filter.page += 1;
-            this.fetch();
-        }
-    });
-
-    App.Model.MovieCollection = MovieCollection;
+	App.Model.MovieCollection = MovieCollection;
 })(window.App);
