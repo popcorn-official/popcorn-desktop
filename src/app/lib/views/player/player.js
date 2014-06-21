0085b5f4298d5a7487e6884f0f1853269a3e5d5b
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-06-21 22:58:27 +1000

    Stop movie pausing on drag
    This was a tough one (not to mention messy!). Basically because videojs's fixEvent mutilates the event object and forces `defaultPrevented` to false, we have to find a hack around it by storing the original event on the DOM element that raised it. We then use this to check if the event is prevented or not. Then clean up and delete the event off the DOM element. We also override the default video click handler in `player.js` to use mouseup instead of mousedown events. Fixes #876

--- src/app/lib/views/player/player.js
+++ src/app/lib/views/player/player.js
@@ -92,6 +92,8 @@
             // Double Click to toggle Fullscreen
             $('#video_player').dblclick(function(event){
                 _this.toggleFullscreen();
+                // Stop any mouseup events pausing video
+                event.preventDefault();
             });
 
             if(this.model.get('type') === 'video/youtube') {
@@ -103,6 +105,23 @@
 
             var player = this.video.player();
             this.player = player;
+
+            /* The following is a hack to make VideoJS listen to
+               mouseup instead of mousedown for pause/play on the 
+               video element. Stops video pausing/playing when
+               dragged. TODO: #fixit! /XC                        */
+            this.player.tech.off('mousedown');
+            this.player.tech.on('mouseup', function(event) {
+                if(event.target.origEvent) {
+                    if(!event.target.origEvent.originalEvent.defaultPrevented) {
+                        _this.player.tech.onClick(event);
+                    }
+                    // clean up after ourselves
+                    delete event.target.origEvent;
+                } else {
+                    _this.player.tech.onClick(event);
+                }
+            });
             
             // Force custom controls
             player.usingNativeControls(false);
