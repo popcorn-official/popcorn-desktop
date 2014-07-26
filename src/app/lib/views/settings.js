9acd8dc4daf8092d741aacff7ca1f1a2b1483a5c
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-07-26 21:29:13 +1000

    settings: only import data if its available

--- src/app/lib/views/settings.js
+++ src/app/lib/views/settings.js
@@ -393,45 +393,46 @@
 							}
 						}
 					}
-				}
-				Database.markEpisodesWatched(watched, function(err, data) {
-					if (err) {
-						win.error(err);
-						$('#syncTrakt').text(i18n.__('Error')).removeClass('disabled').addClass('red');
-						return;
-					}
-					win.info(data.length + ' episodes marked watched');
-					App.Trakt.movie.getWatched().then(function(data) {
-						if (data) {
-							var movie;
-							watched = [];
-							for (var m in data) {
-								movie = data[m];
-								watched.push({
-									movie_id: movie.imdb_id.toString(),
-									date: new Date(),
-									type: 'movie'
-								});
-							}
-							console.log(watched);
-							Database.markMoviesWatched(watched, function(err, data) {
-								if (err) {
-									win.error(err);
-									$('#syncTrakt').text(i18n.__('Error')).removeClass('disabled').addClass('red');
-									return;
-								}
-								win.info(data.length + ' movies marked watched');
-								Database.getUserInfo(function() {
-									$('#syncTrakt').text(i18n.__('Done')).removeClass('disabled').addClass('green');
-									return;
-								});
-							});
-						} else {
-							$('#syncTrakt').text(i18n.__('Done')).removeClass('disabled').addClass('green');
+					
+					Database.markEpisodesWatched(watched, function(err, data) {
+						if (err) {
+							win.error(err);
+							$('#syncTrakt').text(i18n.__('Error')).removeClass('disabled').addClass('red');
+							return;
 						}
+						win.info(data.length + ' episodes marked watched');
+						App.Trakt.movie.getWatched().then(function(data) {
+							if (data) {
+								var movie;
+								watched = [];
+								for (var m in data) {
+									movie = data[m];
+									watched.push({
+										movie_id: movie.imdb_id.toString(),
+										date: new Date(),
+										type: 'movie'
+									});
+								}
+								console.log(watched);
+								Database.markMoviesWatched(watched, function(err, data) {
+									if (err) {
+										win.error(err);
+										$('#syncTrakt').text(i18n.__('Error')).removeClass('disabled').addClass('red');
+										return;
+									}
+									win.info(data.length + ' movies marked watched');
+									Database.getUserInfo(function() {
+										$('#syncTrakt').text(i18n.__('Done')).removeClass('disabled').addClass('green');
+										return;
+									});
+								});
+							} else {
+								$('#syncTrakt').text(i18n.__('Done')).removeClass('disabled').addClass('green');
+							}
 
+						});
 					});
-				});
+				}
 			});
 		}
 	});
