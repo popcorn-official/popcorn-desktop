88b3088e1b5dc52fb370f5554b2bae3b1446517d
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-07-28 21:54:57 +0100

    Fix PT-1022

---
+++ src/app/lib/jquery.plugins.js
@@ -0,0 +1,34 @@
+// jQuery plugi
+(function( $ ){
+
+	// Drag subtitles around player
+    $.fn.drags = function() {
+        var $el = this;
+        return $el.css('cursor', 'move').on("mousedown", function(e) {
+            var $drag = $(this).addClass('draggable'),
+                z_idx = $drag.css('z-index'),
+                drg_h = $drag.outerHeight(),
+                drg_w = $drag.outerWidth(),
+                pos_y = $drag.offset().top + drg_h - e.pageY,
+                pos_x = $drag.offset().left + drg_w - e.pageX,
+                max_y = win.height - 40;
+
+            $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
+                if(e.pageX < 0 || e.pageX > win.width || e.pageY < 0 || e.pageY > win.height) {
+                    $(this).off('mousemove');
+                    $('.draggable').css('z-index', z_idx).removeClass('draggable');
+                } else {
+                    fin_y = e.pageY + pos_y - drg_h;
+                    if(fin_y > 60 && fin_y + $('.draggable').height() < max_y) {
+                        $('.draggable').offset({top:fin_y});
+                        //left:e.pageX + pos_x - drg_w
+                    }
+                }
+            }).on("mouseup", function() {
+                $(this).off('mousemove');
+                $('.draggable').css('z-index', z_idx).removeClass('draggable');
+            });
+            e.preventDefault(); // disable selection
+        });
+    };
+})( jQuery );
