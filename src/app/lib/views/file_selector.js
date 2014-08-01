e7175fe30e04c9f60970535b39f20e3c4391b240
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-08-01 02:09:38 +0200

    Fix filterbar not showing after FileSelector

--- src/app/lib/views/file_selector.js
+++ src/app/lib/views/file_selector.js
@@ -13,13 +13,12 @@
 
         initialize: function() {
             _this = this;
-            Mousetrap.bind('esc', function(e) {
-                _this.closeSelector(e);
-            });
         },
 
         onShow: function() {
-
+			Mousetrap.bind(['esc','backspace'], function(e) {
+                _this.closeSelector(e);
+            });
         },
 
 
@@ -31,16 +30,25 @@
 
             var torrentStart = new Backbone.Model({torrent: torrent, torrent_read: true, file_index: actualIndex});
             App.vent.trigger('stream:start', torrentStart);
-            _this.closeSelector(e);
-        },
-
-        closeSelector: function(e) {
-            e.preventDefault();
             App.vent.trigger('system:closeFileSelector');
         },
 
+        closeSelector: function(e) {
+			Mousetrap.bind('backspace', function(e) {
+				App.vent.trigger('show:closeDetail');
+				App.vent.trigger('movie:closeDetail');
+			});
+			$('.filter-bar').show();
+			$('#header').css('box-shadow', 'none');
+			$('#movie-detail').show();
+			App.vent.trigger('system:closeFileSelector');
+        },
+		
+		onClose: function() {
+
+		},
+		
     });
 
     App.View.FileSelector = FileSelector;
 })(window.App);
-    
