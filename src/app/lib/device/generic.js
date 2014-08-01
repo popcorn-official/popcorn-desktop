1ec019a2748d0815cb7d44964dd82e46ebe72148
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-08-01 11:19:38 +0100

    Fix indentation in generic.js

--- src/app/lib/device/generic.js
+++ src/app/lib/device/generic.js
@@ -1,70 +1,70 @@
 (function(App) {
-    'use strict';
+	'use strict';
 
-    var self;
+	var self;
 
-    var Device = Backbone.Model.extend ({
-        defaults: {
-            type: 'local',
-            name: 'Popcorn Time'
-        },
-        play: function (device, url) {
-            console.error ('PLAY not implemented in generic class');
-        },
-        getID: function () {
-            return this.id;
-        }
+	var Device = Backbone.Model.extend ({
+		defaults: {
+			type: 'local',
+			name: 'Popcorn Time'
+		},
+		play: function (device, url) {
+			console.error ('PLAY not implemented in generic class');
+		},
+		getID: function () {
+			return this.id;
+		}
 
-    });
+	});
 
-    var DeviceCollection = Backbone.Collection.extend ({
-        selected: null,
-        initialize: function () {
-            App.vent.on('device:list', this.list);
-            self = this;
-        },
-        list: function () {
-            _.each(self.models, function (device) {
-                App.vent.trigger('device:add', device);
-            });
-        },
-        startDevice:  function (streamModel) {
-            if (! this.selected) {
-                return false;
-            }
+	var DeviceCollection = Backbone.Collection.extend ({
+		selected: null,
+		initialize: function () {
+			App.vent.on('device:list', this.list);
+			self = this;
+		},
+		list: function () {
+			_.each(self.models, function (device) {
+				App.vent.trigger('device:add', device);
+			});
+		},
+		startDevice:  function (streamModel) {
+			if (! this.selected) {
+				return false;
+			}
 
-            var src = streamModel.get(src);
-            return this.selected.play (src[0].url);
-        }
+			var src = streamModel.get(src);
+			return this.selected.play (src[0].url);
+		}
 
-    });
+	});
 
-    var collection = new DeviceCollection (new Device());
-    var ChooserView = Backbone.Marionette.ItemView.extend({
-        template: '#player-chooser-tpl',
-        events: {'click .playerchoicemenu li a': 'selectPlayer'},
-        onRender: function () {
-            $('.playerchoicemenu li#player-local a').addClass('active');
-        },
-        selectPlayer: function (e) {
-            var player = $(e.currentTarget).parent('li').attr('id').replace('player-', '');
-            this.model.set('device', player);
-            $('.playerchoicemenu li a.active').removeClass('active');
-            $(e.currentTarget).addClass('active');
-            $('.imgplayerchoice').attr('src',  $(e.currentTarget).children('img').attr('src'));
-        }
-    });
+	var collection = new DeviceCollection (new Device());
+	var ChooserView = Backbone.Marionette.ItemView.extend({
+		template: '#player-chooser-tpl',
+		events: {'click .playerchoicemenu li a': 'selectPlayer'},
+		onRender: function () {
+			$('.playerchoicemenu li#player-local a').addClass('active');
+		},
+		selectPlayer: function (e) {
+			var player = $(e.currentTarget).parent('li').attr('id').replace('player-', '');
+			this.model.set('device', player);
+			$('.playerchoicemenu li a.active').removeClass('active');
+			$(e.currentTarget).addClass('active');
+			$('.imgplayerchoice').attr('src',  $(e.currentTarget).children('img').attr('src'));
+		}
+	});
 
-    var createChooserView = function (el) {
-        return new ChooserView ({
-            collection: collection,
-            el: el
-        });
-    };
+	var createChooserView = function (el) {
+		return new ChooserView ({
+			collection: collection,
+			el: el
+		});
+	};
 
-    App.Device = {
-        Generic: Device,
-        Collection: collection,
-        ChooserView: createChooserView
-    };
+	App.Device = {
+		Generic: Device,
+		Collection: collection,
+		ChooserView: createChooserView
+	};
 })(window.App);
