b23b44b533fc75e715688220baf778657403c014
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-08-01 18:51:51 -0300

    add XBMC device support, thanks to Carlos Guerrero

---
+++ src/app/lib/device/xbmc.js
@@ -0,0 +1,35 @@
+(function(App) {
+	'use strict';
+
+	var browser = require( 'airplay-xbmc' ).createBrowser();
+
+	var collection = App.Device.Collection;
+
+        console.log ('hello jimmy');
+	var makeID=  function (baseID) {
+		return 'airplay-xbmc' + baseID;
+	};
+
+	var AirplayXBMC = App.Device.Airplay.extend ({
+		defaults: {
+			type: 'airplay-xbmc'
+		},
+                makeID: makeID
+	});
+
+	browser.on( 'deviceOn', function( device ) {
+                console.log ("new xbmc device",device);
+		collection.add(new AirplayXBMC ({device: device}));
+	});
+
+	browser.on( 'deviceOff', function( device ) {
+		var model = collection.get ({id: makeID(device.id)});
+		if (model) {
+			model.destroy();
+		}
+	});
+
+	browser.start();
+
+	App.Device.AirplayXBMC = AirplayXBMC;
+})(window.App);
