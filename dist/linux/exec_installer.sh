fd9e161e32eb1fa39159a92a09ef32540762ab07
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-08-15 19:08:39 +0200

    fix grunt ><

--- dist/linux/exec_installer.sh
+++ dist/linux/exec_installer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #copy basefile
-cp exec_basefile.sh linux-installer
+cp dist/linux/exec_basefile.sh dist/linux/linux-installer
 
 #get version from package.json
-version=$(cat ../../package.json | sed '/version/!d' | sed s/\"version\"://g | sed s/\"//g | sed s/\ //g | sed s/,//g)
+version=$(cat package.json | sed '/version/!d' | sed s/\"version\"://g | sed s/\"//g | sed s/\ //g | sed s/,//g)
 
 #write version in script
-sed -i "s/PT_VERSION/$version/g" linux-installer
+sed -i "s/PT_VERSION/$version/g" dist/linux/linux-installer
