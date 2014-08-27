e1668ada907c8787766ff568ccabed9b00e0a73d
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-08-27 15:57:10 +0100

    Fix localization in help

--- src/app/lib/views/help.js
+++ src/app/lib/views/help.js
@@ -1,20 +1,6 @@
 (function(App) {
     'use strict';
-
-    var dyks = [
-    i18n.__('You can paste magnet links anywhere in Popcorn Time with CTRL+V'),
-    i18n.__('You can drag & drop a .torrent file into Popcorn Time'),
-    i18n.__('The Popcorn Time project started in February 2014 and has already had 150 people that contributed more than 3000 times to its development in August 2014.'),
-    i18n.__('The rule n°10 applies here.'),
-    i18n.__('If a subtitle for a TV Show is missing, you can add it on <a class="link" href="http://opensubtitles.org">opensubtitles.org</a>. And the same way for a Movie, but on <a class="link" href="http://yifysubtitles.com">yifysubtitles.com</a>'),
-    i18n.__('If you\'re experiencing connection drop issues, try to reduce the DHT Limit in Settings'),
-    i18n.__('Search "1998" to see all movies that came out that year'),
-    i18n.__('You can login to Trakt.tv to save all your watched items, and synchronize them across multiple devices.'),
-    i18n.__('Clicking on the rating stars will display a number instead'),
-    i18n.__('This application is entirely written in HTML5, CSS3 and Javascript'),
-    i18n.__('Wanna know more about a Movie or a TV Series? Just click the IMDb icon.'),
-	i18n.__('Clicking twice on a "Sort By" filter reverses the order of the list')
-    ]
+    var dyks;
 
     var Help = Backbone.Marionette.ItemView.extend({
         template: '#help-tpl',
@@ -26,6 +12,19 @@
         },
 
         initialize: function() {
+            dyks = [
+                i18n.__('You can paste magnet links anywhere in Popcorn Time with CTRL+V'),
+                i18n.__('You can drag & drop a .torrent file into Popcorn Time'),
+                i18n.__('The Popcorn Time project started in February 2014 and has already had 150 people that contributed more than 3000 times to its development in August 2014.'),
+                i18n.__('The rule n°10 applies here.'),
+                i18n.__('If a subtitle for a TV Show is missing, you can add it on <a class="link" href="http://opensubtitles.org">opensubtitles.org</a>. And the same way for a Movie, but on <a class="link" href="http://yifysubtitles.com">yifysubtitles.com</a>'),
+                i18n.__('If you\'re experiencing connection drop issues, try to reduce the DHT Limit in Settings'),
+                i18n.__('Search "1998" to see all movies that came out that year'),
+                i18n.__('You can login to Trakt.tv to save all your watched items, and synchronize them across multiple devices.'),
+                i18n.__('Clicking on the rating stars will display a number instead'),
+                i18n.__('This application is entirely written in HTML5, CSS3 and Javascript'),
+                i18n.__('Wanna know more about a Movie or a TV Series? Just click the IMDb icon.')
+            ];
         },
 
         onShow: function() {
