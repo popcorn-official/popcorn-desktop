9d390d02a618a116622b650a714c7bacc791e18a
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-09-23 22:43:41 +0200

    Added version to callback, added method 'getplaying' and changed 'setvolume' method to 'volume' with ability to get current volume

--- src/app/httpapi.js
+++ src/app/httpapi.js
@@ -8,162 +8,203 @@
 		server = rpc.Server({
 			'headers': { // allow custom headers is empty by default
 				'Access-Control-Allow-Origin': '*'
-			}
+			}			
 		});
 
 		server.expose('ping', function (args, opt, callback) {
-			callback();
+			popcornCallback(callback);
 		});
-
-		server.expose('setvolume', function (args, opt, callback) {
-			var volume = parseFloat(args[0]) || App.Player.volume();
-			App.PlayerView.player.volume(volume);
-			callback();
+		
+		server.expose('volume', function (args, opt, callback) {
+			var volume = 1;
+			if(args.length > 0) {
+				volume = parseFloat(args[0]) || App.Player.volume();
+				App.PlayerView.player.volume(volume);
+			} else {
+				var view = App.PlayerView;
+				if(view !== undefined) {
+					volume = view.player.volume()
+				}
+			}
+			popcornCallback(callback, false, {'volume': volume});
 		});
 
 		server.expose('toggleplaying', function (args, opt, callback) {
 			Mousetrap.trigger('space');
-			callback();
+			popcornCallback(callback);
+		});
+		
+		server.expose('getplaying', function (args, opt, callback) {
+			var view = App.PlayerView;
+			var playing = false;
+			if(view !== undefined && view.player !== undefined && !view.player.paused()) {
+				var result = { 
+					'playing': true, 
+					'title': view.model.get('title'),
+					'movie': view.isMovie(),
+					'quality': view.model.get('quality'),
+					'downloadSpeed': view.model.get('downloadSpeed'),
+					'uploadSpeed': view.model.get('uploadSpeed'),
+					'activePeers': view.model.get('activePeers'),
+					'volume': view.player.volume(),
+					'currentTime': App.PlayerView.player.currentTime(),
+					'duration': App.PlayerView.player.duration()
+				};
+				
+				if(result.movie) {
+					result['imdb_id'] = view.model.get('imdb_id');
+				} else {
+					result['tvdb_id'] = view.model.get('tvdb_id');
+					result['season'] = view.model.get('season');
+					result['episode'] = view.model.get('episode');
+				}
+				
+				popcornCallback(callback, false, result);
+			} else {
+				popcornCallback(callback, false, { 'playing': false });
+			}
 		});
 
 		server.expose('togglemute', function (args, opt, callback) {
 			Mousetrap.trigger('m');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('togglefullscreen', function (args, opt, callback) {
 			Mousetrap.trigger('f');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('togglefavourite', function (args, opt, callback) {
 			Mousetrap.trigger('f');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('togglemoviesshows', function (args, opt, callback) {
 			Mousetrap.trigger('tab');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('togglewatched', function (args, opt, callback) {
 			Mousetrap.trigger('w');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('showslist', function (args, opt, callback) {
 			App.vent.trigger('shows:list');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('movieslist', function (args, opt, callback) {
 			App.vent.trigger('movies:list');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('getviewstack', function (args, opt, callback) {
-			callback(false, [App.ViewStack]);
+			popcornCallback(callback, false, {'viewstack': App.ViewStack});
 		});
 
 		//Filter Bar
 		server.expose('getgenres', function (args, opt, callback) {
-			callback(false, [App.Config.genres]);
+			popcornCallback(callback, false, {'genres': App.Config.genres});
 		});
 
 		server.expose('getgenres_tv', function (args, opt, callback) {
-			callback(false, [App.Config.genres_tv]);
+			popcornCallback(callback, false, {'genres': App.Config.genres_tv});
 		});
 
 		server.expose('getsorters', function (args, opt, callback) {
-			callback(false, [App.Config.sorters]);
+			popcornCallback(callback, false, {'sorters': App.Config.sorters});
 		});
 
 		server.expose('getsorters_tv', function (args, opt, callback) {
-			callback(false, [App.Config.sorters_tv]);
+			popcornCallback(callback, false, {'sorters': App.Config.sorters_tv});
 		});
 
 		server.expose('filtergenre', function (args, opt, callback) {
 			$('.genres .dropdown-menu a[data-value=' + args[0] + ']').click();
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('filtersorter', function (args, opt, callback) {
 			$('.sorters .dropdown-menu a[data-value=' + args[0] + ']').click();
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('filtersearch', function (args, opt, callback) {
 			$('#searchbox').val(args[0]);
 			$('.search form').submit();
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('clearsearch', function (args, opt, callback) {
 			$('.remove-search').click();
+			popcornCallback(callback);
 		});
 
 		//Standard controls
 		server.expose('seek', function (args, opt, callback) {
 			App.PlayerView.seek(parseFloat(args[0]));
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('up', function (args, opt, callback) {
 			Mousetrap.trigger('up');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('down', function (args, opt, callback) {
 			Mousetrap.trigger('down');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('left', function (args, opt, callback) {
 			Mousetrap.trigger('left');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('right', function (args, opt, callback) {
 			Mousetrap.trigger('right');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('enter', function (args, opt, callback) {
 			Mousetrap.trigger('enter');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('back', function (args, opt, callback) {
 			Mousetrap.trigger('backspace');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('quality', function (args, opt, callback) {
 			Mousetrap.trigger('q');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('previousseason', function (args, opt, callback) {
 			Mousetrap.trigger('ctrl+up');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('nextseason', function (args, opt, callback) {
 			Mousetrap.trigger('ctrl+down');
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('subtitleoffset', function (args, opt, callback) {
 			App.PlayerView.adjustSubtitleOffset(parseFloat(args[0]));
-			callback();
+			popcornCallback(callback);
 		});
 
 		server.expose('getsubtitles', function (args, opt, callback) {
-			callback(false, [_.keys(App.MovieDetailView.model.get('subtitle'))]);
+			popcornCallback(callback, false, [_.keys(App.MovieDetailView.model.get('subtitle'))]);
 		});
 
 		server.expose('setsubtitle', function (args, opt, callback) {
 			App.MovieDetailView.switchSubtitle(args[0]);
+			popcornCallback(callback);
 		});
 
 		server.expose('listennotifications', function (args, opt, callback) {
@@ -172,7 +213,7 @@
 			var events = {};
 
 			var emitEvents = function () {
-				callback(false, [events]);
+				popcornCallback(callback, false, {"events": events});
 			};
 
 			//Do a small delay before sending data in case there are more simultaneous events
@@ -226,6 +267,11 @@
 			console.log('socket #' + i + ' destroyed');
 			sockets[i].destroy();
 		}
+	}
+		
+	function popcornCallback(callback, err, result) {
+		result['popcornVersion'] = App.settings.version;
+		callback(err, result);
 	}
 
 	initServer();
