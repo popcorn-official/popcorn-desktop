71c6fddeb9f3c582add609304080c784b2e9d60f
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-09-25 17:07:52 +0100

    I'm able to get shows with next episodes available and show them on the tab, takes a long time tho, next task is to cache and save them (provably as favourites)

--- src/app/lib/providers/watchlist.js
+++ src/app/lib/providers/watchlist.js
@@ -8,7 +8,10 @@
     var queryTorrents = function (filters) {
         var deferred = Q.defer();
 
-        App.db.getBookmarks(filters, function (err, data) {
+        /*App.db.getBookmarks(filters, function (err, data) {
+            deferred.resolve(data || []);
+        });*/
+        App.Trakt.user.getWatched().then(function (data) {
             deferred.resolve(data || []);
         });
 
@@ -16,46 +19,60 @@
     };
 
     var formatForPopcorn = function (items) {
-        var movieList = [];
-
-        items.forEach(function (movie) {
-
-            var deferred = Q.defer();
-            // we check if its a movie 
-            // or tv show then we extract right data
-            if (movie.type === 'movie') {
-                // its a movie
-                Database.getMovie(movie.imdb_id, function (err, data) {
-                    if (data != null) {
-                        data.type = 'bookmarkedmovie';
-                        deferred.resolve(data);
-                    } else {
-                        deferred.reject(err);
-                    }
-                });
-
-            } else {
-                // its a tv show
-                Database.getTVShowByImdb(movie.imdb_id, function (err, data) {
-                    if (data != null) {
-                        data.type = 'bookmarkedshow';
-                        data.image = data.images.poster;
-                        data.imdb = data.imdb_id;
-                        // Fallback for old bookmarks without provider in database
-                        if (typeof (data.provider) === 'undefined') {
-                            data.provider = 'Eztv';
-                        }
-                        deferred.resolve(data);
-                    } else {
-                        deferred.reject(err);
-                    }
-                });
+        var showList = [];
+        console.log('Trakt data: ', items);
+        items.forEach(function (show) {
+            if (! show.next_episode) {
+                console.log('No next episode');
+                return;
             }
+            console.log('Yes next episode', show.show.imdb_id);
+            var deferred = Q.defer();
+            var provider = App.Providers.get('Eztv');
+            var data = provider.detail(show.show.imdb_id,
+                show,
+                function (err, data) {
+                    if (!err) {
+                        data.provider = 'Eztv';
+                        data.type = 'show';
+                        //console.log('Eztv data:', data);
+                        deferred.resolve(data);
+                        /*Database.addTVShow(data, function (err, idata) {
+                            Database.addBookmark(that.model.get('imdb_id'), 'tvshow', function (err, data) {
+                                win.info('Bookmark added (' + that.model.get('imdb_id') + ')');
+                                that.model.set('bookmarked', true);
+                                App.userBookmarks.push(that.model.get('imdb_id'));
+                            });
+                        });*/
 
-            movieList.push(deferred.promise);
+                    } else {
+                        console.log('Error', err);
+                        deferred.reject(err);
+                    }
+                });
+            // its a tv show
+            /*Database.getTVShowByImdb(show.show.imdb_id, function (err, data) {
+                if (data != null) {
+                    console.log('Show found in DB', data);
+                    data.type = 'bookmarkedshow';
+                    data.image = data.images.poster;
+                    data.imdb = data.imdb_id;
+                    // Fallback for old bookmarks without provider in database
+                    if (typeof (data.provider) === 'undefined') {
+                        data.provider = 'Eztv';
+                    }
+                    deferred.resolve(data);
+                } else {
+                    console.log('Show not found in DB');
+                    deferred.reject(err);
+                }
+            });*/
+
+
+            showList.push(deferred.promise);
         });
-
-        return Q.all(movieList);
+        console.log('Showlist', showList);
+        return Q.all(showList);
     };
 
     Watchlist.prototype.extractIds = function (items) {
