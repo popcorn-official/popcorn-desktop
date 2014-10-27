42b41ef69eed0ac7172a359a45100979e7010a9c
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-10-27 08:34:00 +0100

    Changes suggested by SrPatinhas

--- src/app/lib/device/chromecast.js
+++ src/app/lib/device/chromecast.js
@@ -27,11 +27,11 @@
 					url: streamModel.get('cover')
 				},
 				subtitles_style: {
-					backgroundColor: '#FFFFFFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
-					foregroundColor: '#000000FF', // see http://dev.w3.org/csswg/css-color/#hex-notation
-					edgeType: 'DROP_SHADOW', // can be: "NONE", "OUTLINE", "DROP_SHADOW", "RAISED", "DEPRESSED"
-					edgeColor: '#00000000', // see http://dev.w3.org/csswg/css-color/#hex-notation
-					fontScale: 1.5, // transforms into "font-size: " + (fontScale*100) +"%"
+					backgroundColor: '#00000000', // see http://dev.w3.org/csswg/css-color/#hex-notation
+					foregroundColor: '#FFFFFFFF', // see http://dev.w3.org/csswg/css-color/#hex-notation
+					edgeType: 'OUTLINE', // can be: "NONE", "OUTLINE", "DROP_SHADOW", "RAISED", "DEPRESSED"
+					edgeColor: '#000000FF', // see http://dev.w3.org/csswg/css-color/#hex-notation
+					fontScale: 1.3, // transforms into "font-size: " + (fontScale*100) +"%"
 					fontStyle: 'NORMAL', // can be: "NORMAL", "BOLD", "BOLD_ITALIC", "ITALIC",
 					fontFamily: 'Helvetica',
 					fontGenericFamily: 'SANS_SERIF', // can be: "SANS_SERIF", "MONOSPACED_SANS_SERIF", "SERIF", "MONOSPACED_SERIF", "CASUAL", "CURSIVE", "SMALL_CAPITALS",
