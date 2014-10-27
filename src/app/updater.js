35fd7a98b8c426d59ed4ab937b79d193ed0f5c51
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-10-26 20:41:39 -0700

    Revert "Fix updater code for Mac OSX and Windows"
    This reverts commit b49467d3c5405aaf991b6df1ef2ee57d33d7a012.

--- src/app/updater.js
+++ src/app/updater.js
@@ -47,10 +47,7 @@
 			channel: 'beta'
 		});
 
-		this.outputDir = App.settings.os === 'linux' ? process.execPath : 
-			// works regardless of whether or not the app uses a zipped package
-			App.settings.os === 'mac' ? path.join(process.execPath, '../../../../../Resources/app.nw')  :
-			path.join(process.execPath, '../');
+		this.outputDir = App.settings.os === 'linux' ? process.execPath : process.cwd();
 		this.updateData = null;
 	}
 
@@ -154,34 +151,19 @@
 
 	function installWindows(downloadPath, updateData) {
 		var outputDir = path.dirname(downloadPath),
-			nodeModulesDir = path.join(outputDir, 'node_modules'),
-			srcDir = path.join(outputDir, 'src');
+			installDir = path.join(outputDir, 'app');
 		var defer = Q.defer();
 
-		// Remove node modules and src directories
-		rm(nodeModulesDir, function (err) {
+		var pack = new zip(downloadPath);
+		pack.extractAllToAsync(installDir, true, function (err) {
 			if (err) {
 				defer.reject(err);
 			} else {
-				rm(srcDir, function (err) {
+				fs.unlink(downloadPath, function (err) {
 					if (err) {
 						defer.reject(err);
 					} else {
-						// extract update package
-						var pack = new zip(downloadPath);
-						pack.extractAllToAsync(outputDir, true, function (err) {
-							if (err) {
-								defer.reject(err);
-							} else {
-								fs.unlink(downloadPath, function (err) {
-									if (err) {
-										defer.reject(err);
-									} else {
-										defer.resolve();
-									}
-								});
-							}
-						});
+						defer.resolve();
 					}
 				});
 			}
