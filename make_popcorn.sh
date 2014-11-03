e846f22702d055ec7769fee15185e0ebe0b74b04
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-11-02 20:31:40 -0500

    Made the script work cross platform and fixed exiting and error handling issues

--- make_popcorn.sh
+++ make_popcorn.sh
@@ -8,10 +8,25 @@ elif [ "${1}" = "ssh" ]; then
 else
     clone_url="${1}"
 fi
-clone_command () { 
-    git clone ${clone_url} ${dir} && 
-    echo "Cloned Popcorn Time successfully" || 
-    echo "Popcorn Time encountered an error and could not be cloned" && exit 1; 
+
+exec-sudo() {
+    case ${OSTYPE} in msys*)
+       echo $OSTYPE
+       $1
+       ;;
+    *)
+       sudo $1
+       ;;
+    esac
+}
+
+clone_command() {
+    if `git clone ${clone_url} ${dir}`; then
+        echo "Cloned Popcorn Time successfully"
+    else
+        echo "Popcorn Time encountered an error and could not be cloned"
+        exit $?
+    fi
 }
 
 if [ -e ".git/config" ]; then
@@ -20,12 +35,12 @@ if [ -e ".git/config" ]; then
         echo "You appear to be inside of a Popcorn Time repository already, not cloning"
         clone_repo="False"
         ;;
-    *)   
+    *)
         try="True"
         tries=0
         while [ "${try}" = "True" ]; do
             read -p "Looks like we are inside a git repository, do you wish to clone inside it? (yes/no) [no] " rd_cln
-            if [ -z "${rd_cln}" ]; then 
+            if [ -z "${rd_cln}" ]; then
                 rd_cln='no'
             fi
             tries=$((${tries}+1))
@@ -50,20 +65,20 @@ fi
 if [ "${clone_repo}" = "True" ]; then
     echo "Cloning Popcorn Time"
     read -p "Where do you wish to clone popcorn time to? [popcorn-app] " dir
-    if [ -z "${dir}" ]; then 
+    if [ -z "${dir}" ]; then
         dir='popcorn-app'
     elif [ "${dir}" = "/" ]; then
         dir='popcorn-app'
     fi
     if [ ! -d "${dir}" ]; then
         clone_command
-        
+
     else
         try="True"
         tries=0
         while [ "$try" = "True" ]; do
             read -p "Directory ${dir} already exists, do you wish to delete it and redownload? (yes/no) [no] " rd_ans
-            if [ -z "${rd_ans}" ]; then 
+            if [ -z "${rd_ans}" ]; then
                 rd_ans='no'
             fi
             tries=$((${tries}+1))
@@ -95,7 +110,7 @@ try="True"
 tries=0
 while [ "${try}" = "True" ]; do
     read -p "Do you wish to install the required dependencies for Popcorn Time and setup for building? (yes/no) [yes] " rd_dep
-    if [ -z "${rd_dep}" ]; then 
+    if [ -z "${rd_dep}" ]; then
         rd_dep="yes"
     fi
     tries=$((${tries}+1))
@@ -112,27 +127,48 @@ if [ "${rd_dep}" = "yes" ]; then
     if [ -z "${dir}" ]; then
         dir="."
     fi
+    cd ${dir}
+
     echo "Installing global dependencies"
-    sudo npm install -g bower grunt-cli &&
-    cd ${dir} &&
-    echo "Global dependencies installed successfully!" ||
-    echo "Global dependencies encountered an error while installing" && exit 1
+    if exec-sudo "npm install -g bower grunt-cli"; then
+        echo "Global dependencies installed successfully!"
+    else
+        echo "Global dependencies encountered an error while installing"
+        exit $?
+    fi
 
     echo "Installing local dependencies"
-    sudo npm install &&
-    sudo chown -R $USER . &&
-    sudo chown -R $USER ~/.cache &&
-    echo "Local dependencies installed successfully!" ||
-    echo "Local dependencies encountered an error while installing" && exit 1
+    if exec-sudo "npm install"; then
+        echo "Local dependencies installed successfully!"
+    else
+        echo "Local dependencies encountered an error while installing"
+        exit $?
+    fi
+
+    curh=$HOME
+    if exec-sudo "chown -R $USER ." && exec-sudo "chown -R $USER $curh/.cache"; then
+        echo "Local permissions corrected successfully!"
+    else
+        echo "Local permissions encountered an error while correcting"
+        exit $?
+    fi
 
     echo "Setting up Bower"
-    bower install && 
-    echo "Bower successfully installed" ||
-    echo "Encountered an error while installing bower" && exit 1
-    echo "Successfully setup for Popcorn Time"
+    if `bower install`; then
+        echo "Bower successfully installed"
+    else
+        echo "Encountered an error while installing bower"
+        exit $?
+    fi
 
+    echo "Successfully setup for Popcorn Time"
 fi
-grunt build && 
-echo "Popcorn Time built successfully!" && 
-echo "Run 'grunt start' from inside the repository to launch the app" && 
-echo "Enjoy!" || echo "Popcorn Time encountered an error and couldn't be built" && exit 1
+
+if `grunt build`; then
+    echo "Popcorn Time built successfully!"
+    echo "Run 'grunt start' from inside the repository to launch the app"
+    echo "Enjoy!"
+else
+    echo "Popcorn Time encountered an error and couldn't be built"
+    exit $?
+fi
