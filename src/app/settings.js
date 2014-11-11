ddb86e8f65935e30468318f09341cb79ed226278
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-11-11 09:52:39 +0000

    Fix YTS endpoint fingerprints

--- src/app/settings.js
+++ src/app/settings.js
@@ -80,7 +80,7 @@ Settings.updateEndpoint = {
 
 Settings.ytsAPI = {
 	url: 'https://yts.re/api/',
-	fingerprint: 'F4:83:12:02:23:EF:BF:39:4C:85:D3:3C:EE:F2:63:2B:33:43:22:3D',
+	fingerprint: 'D4:7B:8A:2A:7B:E1:AA:40:C5:7E:53:DB:1B:0F:4F:6A:0B:AA:2C:6C',
 	fallbacks: [{
 		url: 'https://yts.pm/api/',
 		fingerprint: 'B6:0A:11:A8:74:48:EB:B4:9A:9C:79:1A:DA:FA:72:BF:F8:8B:0A:B3'
@@ -103,7 +103,7 @@ Settings.ytsAPI = {
 		fingerprint: '77:44:AC:40:4A:B8:A6:83:06:37:5C:56:16:B4:2C:30:B9:75:99:94'
 	}, { // .im is listed last due to lack of ECDSA support in nw0.9.2
 		url: 'https://yts.im/api/',
-		fingerprint: 'F6:E8:18:11:0A:6F:57:7F:F2:9C:FC:C3:80:F9:A8:5B:07:04:08:2C'
+		fingerprint: 'AF:67:49:6C:D5:6A:28:0D:BA:36:C0:12:7D:D9:6E:A7:10:3F:99:14'
 	}]
 };
 
