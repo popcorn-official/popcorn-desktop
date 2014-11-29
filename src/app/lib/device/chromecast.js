1e116d64e042d5b007af3b59be8ce4aed02eb1f7
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-11-29 09:41:08 -0500

    Fix chromecast

--- src/app/lib/device/chromecast.js
+++ src/app/lib/device/chromecast.js
@@ -18,47 +18,59 @@
 			var name = this.get('name');
 			var device = this.get('device');
 			this.set('url', url);
-
-			var media = {
-				url: url,
-				subtitles: [],
-				cover: {
-					title: streamModel.get('title'),
-					url: streamModel.get('cover')
-				},
-				subtitles_style: {
-					backgroundColor: '#00000000', // color of background - see http://dev.w3.org/csswg/css-color/#hex-notation
-					foregroundColor: AdvSettings.get('subtitle_color'), // color of text - see http://dev.w3.org/csswg/css-color/#hex-notation
-					edgeType: AdvSettings.get('subtitle_shadow') ? 'DROP_SHADOW' : 'OUTLINE', // border of text - can be: "NONE", "OUTLINE", "DROP_SHADOW", "RAISED", "DEPRESSED"
-					edgeColor: '#000000FF', // color of border - see http://dev.w3.org/csswg/css-color/#hex-notation
-					fontScale: 1.3, // size of the text - transforms into "font-size: " + (fontScale*100) +"%"
-					fontStyle: 'NORMAL', // can be: "NORMAL", "BOLD", "BOLD_ITALIC", "ITALIC",
-					fontFamily: 'Helvetica',
-					fontGenericFamily: 'SANS_SERIF', // can be: "SANS_SERIF", "MONOSPACED_SANS_SERIF", "SERIF", "MONOSPACED_SERIF", "CASUAL", "CURSIVE", "SMALL_CAPITALS",
-					windowColor: '#00000000', // see http://dev.w3.org/csswg/css-color/#hex-notation
-					windowRoundedCornerRadius: 0, // radius in px
-					windowType: 'NONE' // can be: "NONE", "NORMAL", "ROUNDED_CORNERS"
-				}
-			};
+			var self = this;
+			console.log(url);
+			console.log(device);
 
 			var subtitle = streamModel.get('subFile');
-			var cover = {
-				title: streamModel.get('title'),
-				url: streamModel.get('cover')
-			};
+
 			if (subtitle) {
-				media.subtitles.push({
-					url: 'http:' + url.split(':')[1] + ':9999/subtitle.vtt',
-					name: 'Subtitles',
-					language: 'en-US'
-				});
+				console.log('http:' + url.split(':')[1] + ':9999/subtitle.vtt');
+				var media = {
+					url: url,
+					subtitles: [{
+						url: 'http:' + url.split(':')[1] + ':9999/subtitle.vtt',
+						name: 'Subtitles',
+						language: 'en-US'
+					}],
+					cover: {
+						title: streamModel.get('title'),
+						url: streamModel.get('cover')
+					},
+					subtitles_style: {
+						backgroundColor: '#00000000', // color of background - see http://dev.w3.org/csswg/css-color/#hex-notation
+						foregroundColor: AdvSettings.get('subtitle_color'), // color of text - see http://dev.w3.org/csswg/css-color/#hex-notation
+						edgeType: AdvSettings.get('subtitle_shadow') ? 'DROP_SHADOW' : 'OUTLINE', // border of text - can be: "NONE", "OUTLINE", "DROP_SHADOW", "RAISED", "DEPRESSED"
+						edgeColor: '#000000FF', // color of border - see http://dev.w3.org/csswg/css-color/#hex-notation
+						fontScale: 1.3, // size of the text - transforms into "font-size: " + (fontScale*100) +"%"
+						fontStyle: 'NORMAL', // can be: "NORMAL", "BOLD", "BOLD_ITALIC", "ITALIC",
+						fontFamily: 'Helvetica',
+						fontGenericFamily: 'SANS_SERIF', // can be: "SANS_SERIF", "MONOSPACED_SANS_SERIF", "SERIF", "MONOSPACED_SERIF", "CASUAL", "CURSIVE", "SMALL_CAPITALS",
+						windowColor: '#00000000', // see http://dev.w3.org/csswg/css-color/#hex-notation
+						windowRoundedCornerRadius: 0, // radius in px
+						windowType: 'NONE' // can be: "NONE", "NORMAL", "ROUNDED_CORNERS"
+					}
+				};
+			} else {
+				media = {
+					url: url,
+					cover: {
+						title: streamModel.get('title'),
+						url: streamModel.get('cover')
+					}
+				}
+				console.log(media);
 			}
+
 			device.connect();
 			device.on('connected', function () {
-				device.play(media, function (err, status) {
+				device.play(media, 0, function (err, status) {
+					console.log(err);
+					console.log(device);
 					console.log('Playing ' + url + ' on ' + name);
 				});
 			});
+
 		},
 
 		pause: function () {
