34fca1198c2145642e45fe05e7e6c6539e3a8909
Some Popcorn Time Contributor <pochoclin@popcorntime>
2014-12-24 13:17:13 +0100

    airplay bugfixes

--- src/app/lib/device/airplay.js
+++ src/app/lib/device/airplay.js
@@ -15,7 +15,6 @@
 		},
 		makeID: makeID,
 		initialize: function (attrs) {
-			this.bctl = require('airplay-js').createBrowser();
 			this.device = attrs.device;
 			this.attributes.name = this.device.name || this.device.serverInfo.model;
 			this.attributes.id = this.makeID(this.device.serverInfo.deviceId);
@@ -23,11 +22,9 @@
 		play: function (streamModel) {
 			var url = streamModel.attributes.src;
 			this.device.play(url);
-			this.bctl.start();
 		},
 		stop: function () {
 			this.device.stop(function () {});
-			this.bctl.stop();
 		}
 	});
 
@@ -37,10 +34,6 @@
 			device: device
 		}));
 	});
-	browser.on('ready', function () {
-		browser.discover();
-	});
-
 
 	browser.on('deviceOff', function (device) {
 		var model = collection.get({
