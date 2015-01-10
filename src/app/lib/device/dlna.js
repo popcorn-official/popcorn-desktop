dee6f42e6fac8fdd2b2cb0aa77a03f82cd2d662b
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-01-10 17:02:11 +0100

    -Better DLNA detection

--- src/app/lib/device/dlna.js
+++ src/app/lib/device/dlna.js
@@ -1,12 +1,33 @@
 (function (App) {
 	'use strict';
 
-	var UpnpC = require('dlna-js').ControlPoint;
 	var MediaRendererClient = require('upnp-mediarenderer-client');
 	var collection = App.Device.Collection;
-	var control = UpnpC.UpnpControlPoint;
-	var cp = new control();
-
+	var getIPAddress= function () {
+		var ip, alias = 0;
+		var ifaces = require('os').networkInterfaces();
+		for (var dev in ifaces) {
+			ifaces[dev].forEach(function (details) {
+				if (details.family === 'IPv4') {
+					if (!/(loopback|vmware|internal|hamachi|vboxnet)/gi.test(dev + (alias ? ':' + alias : ''))) {
+						if (details.address.substring(0, 8) === '192.168.' ||
+							details.address.substring(0, 7) === '172.16.' ||
+							details.address.substring(0, 5) === '10.0.'
+						) {
+							ip = details.address;
+							++alias;
+						}
+					}
+				}
+			});
+		}
+		return ip;
+	};
+	var ip= getIPAddress();
+	var ssdp = require('node-ssdp').Client
+	, client = new ssdp({
+		unicastHost: ip
+	})
 	var makeID = function (baseID) {
 		return 'dlna-' + baseID.replace(':', '');
 	};
@@ -19,11 +40,15 @@
 		makeID: makeID,
 
 		initialize: function (attrs) {
+			var self = this;
+			this.info = attrs.info;
 			this.device = attrs.device;
-			this.client = new MediaRendererClient(this.device.location);
-			this.attributes.id = this.makeID(this.device.location);
-			this.attributes.name = this.device.friendlyName;
-			this.attributes.address = this.device.host;
+			this.client = new MediaRendererClient(this.device.LOCATION);
+			this.client.getDeviceDescription(function(err, description) {
+				if(err) throw err;
+				self.attributes.name = description.friendlyName;
+			});
+			self.attributes.address = this.info.address;
 		},
 		play: function (streamModel) {
 			var url = streamModel.get('src');
@@ -62,27 +87,24 @@
 
 	});
 
-	function filterDevice(device) {
-		var type = device.deviceType.replace('urn:schemas-upnp-org:device:', '');
-		type = (type.split(':')[0]);
+	client.on('notify', function () {
+		console.log('Got a notification.')
+	})
 
-		if (type !== 'MediaRenderer') {
-			return null;
+	client.on('response', function inResponse(headers, code, rinfo) {
+		var deviceId = makeID(headers.USN);
+		if (collection.where({id: deviceId}).length === 0) {
+			collection.add(new Dlna({
+				id: deviceId,
+				info: rinfo,
+				device: headers
+			}));
 		}
-		return device;
-	}
-
-
-	cp.on('device', function (device) {
-		if (!filterDevice(device)) {
-			return;
-		}
-		collection.add(new Dlna({
-			device: device
-		}));
 	});
 
-	cp.search();
+	setInterval(function() {
+		client.search('urn:schemas-upnp-org:device:MediaRenderer:1')
+	}, 10000)
 
 
 	App.Device.Dlna = Dlna;
