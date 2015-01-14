4bff691bd5dd0628ad6d6ce435d5d455b7c548f8
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-01-14 09:41:54 -0500

    Fix conflict in last merge

--- src/app/httpapi.js
+++ src/app/httpapi.js
@@ -675,7 +675,7 @@
 
 	App.vent.on('initHttpApi', function () {
 		console.log('Reiniting server');
-		Q.fcall(initServer)
+		Q(initServer)
 			.then(function () {
 				server.enableAuth(Settings.httpApiUsername, Settings.httpApiPassword);
 				if (httpServer) {
