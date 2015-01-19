7b62a09fc3cd87fa8f91b83ac33ef0883d898c95
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-01-19 12:50:39 -0500

    No need to check YTS endpoints with this change

--- src/app/database.js
+++ src/app/database.js
@@ -490,7 +490,6 @@ var Database = {
 				App.vent.trigger('initHttpApi');
 
 				return AdvSettings.checkApiEndpoints([
-					Settings.ytsAPI,
 					Settings.tvshowAPI,
 					Settings.updateEndpoint
 				]);
