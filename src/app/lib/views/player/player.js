03a467009420e0502618cd45205b7043248fc9ac
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-02-08 23:14:18 +0100

    use videojs in fs if app was fs

--- src/app/lib/views/player/player.js
+++ src/app/lib/views/player/player.js
@@ -290,6 +290,10 @@
 			if (AdvSettings.get('alwaysFullscreen') && !this.inFullscreen) {
 				this.toggleFullscreen();
 			}
+			if (this.inFullscreen) {
+				this.player.isFullscreen(true);
+				this.player.trigger('fullscreenchange');
+			}
 			
 			this.player.volume(AdvSettings.get('playerVolume'));
 		},
