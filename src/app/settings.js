60cfdc91edb52b4715534e2e54671485b78da0ab
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-02-16 04:12:16 -0800

    Remove YTS API URL settings
    These might come back later but with the new system I highly doubt it.

--- src/app/settings.js
+++ src/app/settings.js
@@ -104,31 +104,6 @@ Settings.updateEndpoint = {
 	}]
 };
 
-Settings.ytsAPI = {
-	url: 'https://yts.re/api/',
-	index: 0,
-	proxies: [{
-		url: 'https://yts.re/api/',
-		fingerprint: 'D4:7B:8A:2A:7B:E1:AA:40:C5:7E:53:DB:1B:0F:4F:6A:0B:AA:2C:6C',
-	}, {
-		url: 'https://yts.pm/api/',
-		fingerprint: 'B6:0A:11:A8:74:48:EB:B4:9A:9C:79:1A:DA:FA:72:BF:F8:8B:0A:B3'
-	}, {
-		url: 'https://yts.io/api/',
-		fingerprint: '27:96:21:06:E3:2F:5D:3D:7D:46:13:EF:42:5B:AD:5E:C8:FD:DA:45'
-	}, {
-		url: 'https://yts-proxy.net/api/',
-		fingerprint: '8E:49:5B:A9:2E:F1:AE:E8:A2:BB:E2:77:E9:C3:BC:D4:5D:4B:66:1F'
-	}, {
-		url: 'http://proxy.piratenpartij.nl/yts.re/api/',
-		ssl: false,
-		fingerprint: /Piratenpartij.nl Proxy/
-	}, { // .wf is listed last due to lack of ECDSA support in nw0.9.2
-		url: 'https://yts.wf/api/',
-		fingerprint: '77:44:AC:40:4A:B8:A6:83:06:37:5C:56:16:B4:2C:30:B9:75:99:94'
-	}]
-};
-
 // App Settings
 Settings.version = false;
 Settings.dbversion = '0.1.0';
