2660d1aaa0767b08fae34fedfd87d2aab91bfb70
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-02-27 03:36:31 -0300

    Implemented streaming of subtitles over DLNA/UPnP
    issue #160
    - added xmlbuilder dependency to package.json, used to build the xml metadata
    - different vendors, services and devices implement the retrieval of subtitles over UPnP in different ways
    - for maximum compatibility, the metadata contains the video and the subtitle URIs in several places
    - the metadata is sent together with the URI of the video file to the UPnP client object loader
    - regarding UPnP protocol, what is done is filling the CurrentURIMetaData parameter of the SetAVTransportURI function of the AVTransport

--- src/app/lib/device/dlna.js
+++ src/app/lib/device/dlna.js
@@ -3,6 +3,7 @@
 
 	var UpnpC = require('dlna-js').ControlPoint;
 	var MediaRendererClient = require('upnp-mediarenderer-client');
+	var xmlb = require('xmlbuilder');
 	var collection = App.Device.Collection;
 	var control = UpnpC.UpnpControlPoint;
 	var cp = new control();
@@ -27,7 +28,53 @@
 		},
 		play: function (streamModel) {
 			var url = streamModel.get('src');
-			this.client.load(url + 'video.mp4', {
+			var url_video = url + 'video.mp4';
+			var url_subtitle = 'http:' + url.split(':')[1] + ':9999/video.srt';
+			var metadata = null;
+			var subtitle = streamModel.get('subFile');
+			if (subtitle) {
+				metadata = xmlb.create('DIDL-Lite', {
+						'headless': true
+					})
+					.att({
+						'xmlns': 'urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/',
+						'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
+						'xmlns:upnp': 'urn:schemas-upnp-org:metadata-1-0/upnp/',
+						'xmlns:dlna': 'urn:schemas-dlna-org:metadata-1-0/',
+						'xmlns:sec': 'http://www.sec.co.kr/',
+						'xmlns:xbmc': 'urn:schemas-xbmc-org:metadata-1-0/'
+					})
+					.ele('item', {
+						'id': '0',
+						'parentID': '-1',
+						'restricted': '1'
+					})
+					.ele('dc:title', {}, 'Popcorn Time Video')
+					.insertAfter('res', {
+						'protocolInfo': 'http-get:*:video/mp4:',
+						'xmlns:pv': 'http://www.pv.com/pvns/',
+						'pv:subtitleFileUri': url_subtitle,
+						'pv:subtitleFileType': 'srt'
+					}, url_video)
+					.insertAfter('res', {
+						'protocolInfo': 'http-get:*:text/srt:'
+					}, url_subtitle)
+					.insertAfter('res', {
+						'protocolInfo': 'http-get:*:smi/caption'
+					}, url_subtitle)
+					.insertAfter('sec:CaptionInfoEx', {
+						'sec:type': 'srt'
+					}, url_subtitle)
+					.insertAfter('sec:CaptionInfo', {
+						'sec:type': 'srt'
+					}, url_subtitle)
+					.insertAfter('upnp:class', {}, 'object.item.videoItem.movie')
+					.end({
+						pretty: false
+					});
+			}
+			this.client.load(url_video, {
+				metadata: metadata,
 				autoplay: true,
 				contentType: 'video/vnd.dlna.mpeg-tts'
 			}, function (err, result) {
