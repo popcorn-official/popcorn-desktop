99220643c7e83f77de90316af64f0f14fec84f99
Some Popcorn Time Contributor <pochoclin@popcorntime>
2015-03-20 22:47:01 +0200

    Fix DLNA streaming to LG webOS TV
    closes #8 DLNA cast to LG TVs
    closes #170 popcorn time streaming issue to LG smart tv+
    - LG TV needs CurrentURIMetaData protocolInfo along with
      AVTransportURI/CurrentURI

--- src/app/lib/device/dlna.js
+++ src/app/lib/device/dlna.js
@@ -51,7 +51,7 @@
 					})
 					.ele('dc:title', {}, 'Popcorn Time Video')
 					.insertAfter('res', {
-						'protocolInfo': 'http-get:*:video/mp4:',
+						'protocolInfo': 'http-get:*:video/mp4:*',
 						'xmlns:pv': 'http://www.pv.com/pvns/',
 						'pv:subtitleFileUri': url_subtitle,
 						'pv:subtitleFileType': 'srt'
@@ -68,6 +68,31 @@
 					.insertAfter('sec:CaptionInfo', {
 						'sec:type': 'srt'
 					}, url_subtitle)
+					.insertAfter('upnp:class', {}, 'object.item.videoItem.movie')
+					.end({
+						pretty: false
+					});
+			} else {
+				metadata = xmlb.create('DIDL-Lite', {
+						'headless': true
+					})
+					.att({
+						'xmlns': 'urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/',
+						'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
+						'xmlns:upnp': 'urn:schemas-upnp-org:metadata-1-0/upnp/',
+						'xmlns:dlna': 'urn:schemas-dlna-org:metadata-1-0/',
+						'xmlns:sec': 'http://www.sec.co.kr/',
+						'xmlns:xbmc': 'urn:schemas-xbmc-org:metadata-1-0/'
+					})
+					.ele('item', {
+						'id': '0',
+						'parentID': '-1',
+						'restricted': '1'
+					})
+					.ele('dc:title', {}, 'Popcorn Time Video')
+					.insertAfter('res', {
+						'protocolInfo': 'http-get:*:video/mp4:*',
+					}, url_video)
 					.insertAfter('upnp:class', {}, 'object.item.videoItem.movie')
 					.end({
 						pretty: false
