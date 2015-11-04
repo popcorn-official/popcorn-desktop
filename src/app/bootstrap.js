61fcbcd24b94d4b6cd2582d037629e452b548483
Niv Sardi <xaiki@evilgiggle.com>
2015-11-04 04:04:30 -0300

    init all providers on startup
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,4 +1,9 @@
 (function (App) {
     'use strict';
     App.start();
+
+    ['movie', 'tvshow', 'anime', 'indie'].forEach (function (p) {
+        App.Config.getProvider(p);
+    })
+
 })(window.App);
