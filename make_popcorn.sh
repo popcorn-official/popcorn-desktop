9a27a1004ce844533d76d29c2d8877c6a6f98c54
Charles-Eugene Loubao <charlesloubao95@gmail.com>
2016-03-12 08:21:57 -0500

    Added line accidentally removed

--- make_popcorn.sh
+++ make_popcorn.sh
@@ -188,6 +188,7 @@ fi
 if grunt build; then
     echo "Popcorn Time built successfully!"
     ./Create-Desktop-Entry
+    echo "Run 'grunt start' from inside the repository to launch the app"
     echo "Enjoy!"
 else
     echo "Popcorn Time encountered an error and couldn't be built"
