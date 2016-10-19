b54ef32390041b3407c75ae976f8d2c749534a64
vzamanillo <bellzebu@hotmail.com>
2016-10-19 18:22:25 +0200

    Improved switchRating code
    Improved switchRating code (sorted) for movies and TV shows

--- src/app/lib/views/movie_detail.js
+++ src/app/lib/views/movie_detail.js
@@ -183,15 +183,9 @@
         },
 
         switchRating: function () {
-            if ($('.number-container').hasClass('hidden')) {
-                $('.number-container').removeClass('hidden');
-                $('.star-container').addClass('hidden');
-                AdvSettings.set('ratingStars', false);
-            } else {
-                $('.number-container').addClass('hidden');
-                $('.star-container').removeClass('hidden');
-                AdvSettings.set('ratingStars', true);
-            }
+            $('.number-container').toggleClass('hidden');
+            $('.star-container').toggleClass('hidden');
+            AdvSettings.set('ratingStars', $('.number-container').hasClass('hidden'));
         },
 
         switchSubtitle: function (lang) {
