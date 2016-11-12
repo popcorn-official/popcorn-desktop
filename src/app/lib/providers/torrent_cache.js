522b815f0ba0b366bc12aea80d5266e2e876360c
vankasteelj <vankasteelj@gmail.com>
2016-11-12 17:50:53 +0100

    remove error on torrent_cache tmp folder creation (if exists)

--- src/app/lib/providers/torrent_cache.js
+++ src/app/lib/providers/torrent_cache.js
@@ -136,11 +136,7 @@
     };
 
     pmod._checkTmpDir = function () {
-        mkdirp(tpmDir, function (err) {
-            if (err) {
-                win.error('TorrentCache._checkTmpDir()', err);
-            }
-        });
+        mkdirp(tpmDir);
     };
 
     pmod.getType = function (torrent) {
