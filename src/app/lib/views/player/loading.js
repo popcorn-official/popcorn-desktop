4bbd0ee74db2961ef39b184e9bcf5afb690d587d
vankasteelj <vankasteelj@gmail.com>
2016-11-12 18:08:36 +0100

    remove commented out line

--- src/app/lib/views/player/loading.js
+++ src/app/lib/views/player/loading.js
@@ -119,8 +119,6 @@
                 // The 'downloading' state is not always sent, eg when playing canceling and replaying
                 // Start listening here instead when playing externally
                 this.listenTo(this.model.get('streamInfo'), 'change:downloaded', this.onProgressUpdate);
-                // The first progress update can take some time, so force updating the UI immediately
-                //this.onProgressUpdate();
             }
         },
 
