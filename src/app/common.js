4c141633b33d481da0396b0b39bfd7e3e0815585
vankasteelj <vankasteelj@gmail.com>
2016-11-12 21:54:20 +0100

    add Common.normalize()

--- src/app/common.js
+++ src/app/common.js
@@ -442,3 +442,26 @@ Common.sanitize = function (input) {
 
     return output;
 };
+
+Common.normalize = (function () {
+    var from = 'ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç'; 
+    var to =   'AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc';
+    var mapping = {};
+
+    for (var i = 0, j = from.length; i < j; i++) {
+        mapping[from.charAt(i)] = to.charAt(i);
+    }
+
+    return function (str) {
+        var ret = [];
+        for (var i = 0, j = str.length; i < j; i++) {
+            var c = str.charAt(i);
+            if (mapping.hasOwnProperty(str.charAt(i))) {
+                ret.push(mapping[c]);
+            } else {
+                ret.push(c);
+            }
+        }
+        return ret.join('').replace(/[^\w,'-]/g, '');
+    };
+})();
