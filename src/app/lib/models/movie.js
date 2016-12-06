d64c482f0c1f750ad6b89986c1cafd859a0a9818
Niv Sardi <xaiki@evilgiggle.com>
2016-12-05 21:20:30 -0300

    move content_item to it's own file
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/app/lib/models/movie.js
+++ src/app/lib/models/movie.js
@@ -1,37 +1,7 @@
 (function (App) {
     'use strict';
 
-    var ContentItem = Backbone.Model.extend({
-        events: {
-            'change:torrents': 'updateHealth'
-        },
-
-        idAttribute: 'imdb_id',
-
-        initialize: function (attrs) {
-            this.set('providers', Object.assign(attrs.providers,
-                                                this.getProviders()));
-            this.updateHealth();
-        },
-
-        getProviders: function() {
-            return {};
-        },
-
-        updateHealth: function () {
-            var torrents = this.get('torrents');
-
-            _.each(torrents, function (torrent) {
-                torrent.health = Common.healthMap[Common.calcHealth(torrent)];
-            });
-
-            this.set('torrents', torrents, {
-                silent: true
-            });
-        }
-    });
-
-    var Movie = ContentItem.extend({
+    var Movie = App.ContentItem.extend({
         getProviders: function() {
             return {
                 subtitle: App.Config.getProviderForType('subtitle')
@@ -39,6 +9,5 @@
         }
     });
 
-    App.Model.ContentItem = ContentItem;
     App.Model.Movie = Movie;
 })(window.App);
