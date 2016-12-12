aaa8a9ddcd69172673bd88bdcf42191c62501bba
Niv Sardi <xaiki@evilgiggle.com>
2016-12-12 11:33:40 -0300

    resolve ip before checking
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/app/lib/device/generic.js
+++ src/app/lib/device/generic.js
@@ -2,6 +2,8 @@
     'use strict';
 
     var self;
+    var dns = require('dns'),
+        os = require('os');
 
     // Supports both IPv4 and IPv6 comparison
     var _sequentialPartsInCommon = function (ip1, ip2) {
@@ -45,27 +47,42 @@
              * Supports IPv4 and IPv6.
              */
             if (this.get('typeFamily') !== 'external') {
-                return streamModel;
+                return Promise.resolve(streamModel);
             }
 
             //console.warn('External Device ', this.selected);
             var ips = [],
-                ifaces = os.networkInterfaces();
+                ifaces = os.networkInterfaces(),
+                ipFamily = this.get('ipFamily');
             for (var dev in ifaces) {
                 ifaces[dev].forEach(details => {
-                    if (!details.internal && details.family.match(this.ipFamily)) {
+                    if (!details.internal && details.family.match(ipFamily)) {
                         ips.push(details.address);
                     }
                 });
             }
-            var deviceIp = this.selected.get('address');
-            console.log('Device IP: ' + deviceIp);
-            console.log('Available IPs: ' + JSON.stringify(ips));
-            var srcIp = _getClosestIP(ips, deviceIp);
-            console.log('%s picked for external playback', srcIp);
-            var smIp = streamModel.get('src');
-            streamModel.set('src', smIp.replace('127.0.0.1', srcIp));
-            return streamModel;
+
+            return new Promise((resolve, reject) => {
+                // XXX(xaiki): we'd need a better way to check what we want
+                var options = ipFamily.mach('4') ? {family: 4} :
+                    ipFamily.mach('6') ? {family: 6} :
+                    {};
+
+                dns.lookup(this.selected.get('address'), options,
+                           (err, deviceIp, family) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    console.log('Device IP: ' + deviceIp);
+                    console.log('Available IPs: ' + JSON.stringify(ips));
+                    var srcIp = _getClosestIP(ips, deviceIp);
+                    console.log('%s picked for external playback', srcIp);
+                    var smIp = streamModel.get('src');
+                    streamModel.set('src', smIp.replace('127.0.0.1', srcIp));
+                    return resolve(streamModel);
+                });
+            });
         }
     });
 
@@ -121,8 +138,8 @@
                 this.selected = this.models[0];
             }
 
-            streamModel = this.selected.setIP(streamModel);
-            return this.selected.play(streamModel);
+            this.selected.setIP(streamModel)
+                .then(this.selected.play.bind(this));
         },
 
         setDevice: function (deviceID) {
