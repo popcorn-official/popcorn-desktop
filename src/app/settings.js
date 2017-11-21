b9954d8eba9581310d749cde4929b67d06e60ebc
Charles Barcza <info@blackpanther.hu>
2017-11-21 02:33:57 +0100

    Important modification for system stability if use TMPFS

--- src/app/settings.js
+++ src/app/settings.js
@@ -164,7 +164,7 @@ Settings.opensubtitlesPassword = '';
 // Advanced options
 Settings.connectionLimit = 55;
 Settings.streamPort = 0; // 0 = Random
-Settings.tmpLocation = path.join(os.tmpDir(), Settings.projectName);
+Settings.tmpLocation = path.join(os.homedir(), '/.cache', Settings.projectName);
 Settings.databaseLocation = path.join(data_path, 'data');
 Settings.deleteTmpOnClose = true;
 Settings.automaticUpdating = true;
