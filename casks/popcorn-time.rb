5c68847eb91e55d6264485828f2a37a463828ec8
team-pct <teampct@tuta.io>
2020-03-18 12:59:45 +0100

    Update popcorn-time.rb

--- casks/popcorn-time.rb
+++ casks/popcorn-time.rb
@@ -1,6 +1,6 @@
 cask 'popcorn-time' do
   version '0.4.0'
-  sha256 '62002814ddac7587e447d0f8e7db5d7359ccca77893b79223f7b1f5c7d8992bf'
+  sha256 'c033562ea9a729f53798834bc721d0d5911bafe8c5a809138213f078c328ccd4'
 
   url "https://mirror03.popcorntime.sh/repo/build/Popcorn-Time-#{version}.pkg"
   appcast 'https://github.com/popcorn-official/popcorn-desktop/releases.atom'
