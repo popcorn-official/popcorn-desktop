79a8c3b95dbc97c15b5991f630ba3a52d5ecde87
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2020-04-04 22:14:36 +0300

    move init provider language

--- src/app/database.js
+++ src/app/database.js
@@ -408,9 +408,6 @@ var Database = {
                 if (Settings.version === false) {
                     window.__isNewInstall = true;
                 }
-                if (Settings.language) {
-                    App.Providers.updateLanguage(Settings.language);
-                }
 
                 if (Settings.customMoviesServer || Settings.customSeriesServer || Settings.customAnimeServer || Settings.proxyServer) {
                   App.Providers.updateConnection(Settings.customMoviesServer, Settings.customSeriesServer, Settings.customAnimeServer, Settings.proxyServer);
@@ -429,6 +426,9 @@ var Database = {
                 window.setLanguage(Settings.language);
                 // set hardware settings and usefull stuff
                 return AdvSettings.setup();
+            })
+            .then(function () {
+                App.Providers.updateLanguage(Settings.language);
             })
             .then(function () {
                 App.Trakt = App.Config.getProviderForType('metadata');
