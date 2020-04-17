e7710ba271af90761f79735b3de9162b5beda545
tlsssl <vcs.tlsssl@fastmail.com>
2020-04-17 17:07:16 +0000

    use HTTPS when opening imdb and subtitle websites

--- src/app/lib/views/help.js
+++ src/app/lib/views/help.js
@@ -16,7 +16,7 @@
                 i18n.__('You can paste magnet links anywhere in %s with CTRL+V.', Settings.projectName),
                 i18n.__('You can drag & drop a .torrent file into %s.', Settings.projectName),
                 i18n.__('The %s project was forked from the Popcorn Time project that started in February 2014 and has already had 150 people that contributed more than 3000 times to it\'s development in August 2014.', Settings.projectName),
-                i18n.__('If a subtitle for a TV series is missing, you can add it on %s. And the same way for a movie, but on %s.', '<a class="links" href="http://opensubtitles.org">opensubtitles.org</a>', '<a class="links" href="http://yifysubtitles.com">yifysubtitles.com</a>'),
+                i18n.__('If a subtitle for a TV series is missing, you can add it on %s. And the same way for a movie, but on %s.', '<a class="links" href="https://www.opensubtitles.org">opensubtitles.org</a>', '<a class="links" href="https://www.yifysubtitles.com">yifysubtitles.com</a>'),
                 i18n.__('If you\'re experiencing connection drop issues, try to reduce the DHT Limit in settings.'),
                 i18n.__('If you search \"1998\", you can see all the movies or TV series that came out that year.'),
                 i18n.__('You can login to Trakt.tv to save all your watched items, and synchronize them across multiple devices.'),
