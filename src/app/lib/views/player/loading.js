af511e49313c7c8bb6c3a5d61436a8ab5e0e59a5
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2020-07-28 03:33:45 +0300

    Update loading.js

--- src/app/lib/views/player/loading.js
+++ src/app/lib/views/player/loading.js
@@ -218,9 +218,9 @@
       if (state === 'playingExternally') {
         this.ui.progressbar.hide();
         if (streamInfo && streamInfo.get('device')) {
-          this.ui.cancel_button.css('visibility', 'visible');
           this.ui.vpn.css('display', 'none');
           this.ui.playingbar.css('width', '0%');
+          this.ui.cancel_button.css('visibility', 'visible');
           if (Settings.activateLoCtrl === true) {
             $('.show-pcontrols').removeClass('fa-angle-down').addClass('fa-angle-up').attr("data-original-title", i18n.__('Hide playback controls'));
             this.ui.cancel_button.css('display', 'none');
