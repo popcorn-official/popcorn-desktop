97f6b186f70cd3b934ff15415c21e42be5acb5ae
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2020-08-06 07:23:28 +0300

    Fix some weird behavior when directly switching from the Torrent Collection to the Seedbox and vice versa

--- src/app/lib/views/browser/filter_bar.js
+++ src/app/lib/views/browser/filter_bar.js
@@ -369,14 +369,13 @@
     showTorrentCollection: function(e) {
       e.preventDefault();
       if (App.currentview !== 'Torrent-collection') {
-        if (App.currentview !== 'Seedbox') {
-          App.previousview = App.currentview;
-        } else {
-          App.previousview = 'Movies';
+        if (App.currentview === 'Seedbox') {
+          App.currentview = App.previousview;
+          App.vent.trigger('seedbox:close');
         }
+        App.previousview = App.currentview;
         App.currentview = 'Torrent-collection';
         App.vent.trigger('about:close');
-        App.vent.trigger('seedbox:close');
         App.vent.trigger('torrentCollection:show');
         this.setActive('Torrent-collection');
       } else {
@@ -389,14 +388,13 @@
     showSeedbox: function(e) {
       e.preventDefault();
       if (App.currentview !== 'Seedbox') {
-        if (App.currentview !== 'Torrent-collection') {
-          App.previousview = App.currentview;
-        } else {
-          App.previousview = 'Movies';
+        if (App.currentview === 'Torrent-collection') {
+          App.currentview = App.previousview;
+          App.vent.trigger('torrentCollection:close');
         }
+        App.previousview = App.currentview;
         App.currentview = 'Seedbox';
         App.vent.trigger('about:close');
-        App.vent.trigger('torrentCollection:close');
         App.vent.trigger('seedbox:show');
         this.setActive('Seedbox');
       } else {
