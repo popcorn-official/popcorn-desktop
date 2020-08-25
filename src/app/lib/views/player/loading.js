06fa419a041e942dac1f8aaa51c930cfef7cbb66
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2020-08-25 09:05:17 +0300

    Get rid of some loading screen errors

--- src/app/lib/views/player/loading.js
+++ src/app/lib/views/player/loading.js
@@ -200,12 +200,14 @@
       win.info('Loading torrent:', state);
 
       this.ui.stateTextDownload.text(i18n.__(state));
-      if (streamInfo.get('src') && Settings.ipAddress) {
-        this.ui.stateTextStreamUrl.text(streamInfo.get('src').replace('127.0.0.1', Settings.ipAddress));
+      if (streamInfo) {
+        if (streamInfo.get('src') && Settings.ipAddress) {
+          this.ui.stateTextStreamUrl.text(streamInfo.get('src').replace('127.0.0.1', Settings.ipAddress));
+        }
+        this.ui.stateTextFilename.text(streamInfo.get('filename'));
+        this.ui.stateTextSize.text(Common.fileSize(streamInfo.get('size')));
+        this.ui.stateTextDownloadedFormatted.text(Common.fileSize(streamInfo.get('downloaded')) + ' / ');
       }
-      this.ui.stateTextFilename.text(streamInfo.get('filename'));
-      this.ui.stateTextSize.text(Common.fileSize(streamInfo.get('size')));
-      this.ui.stateTextDownloadedFormatted.text(Common.fileSize(streamInfo.get('downloaded')) + ' / ');
       this.listenTo(this.model.get('streamInfo'), 'change', this.onInfosUpdate);
 
       if (state === 'downloading') {
