a46900458734e8a21b4852d95b62fbd06c40f69b
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2020-10-13 10:40:29 +0300

    Add Crop to Fit screen keyboard shortcut

--- src/app/lib/views/player/player.js
+++ src/app/lib/views/player/player.js
@@ -662,6 +662,10 @@
                 that.closePlayer();
             });
 
+            Mousetrap.bind(['c', 'C'], function (e) {
+                that.toggleCrop();
+            }, 'keydown');
+
             Mousetrap.bind(['v', 'V'], function (e) {
                 that.subtitlesOnOff();
             }, 'keydown');
@@ -817,6 +821,8 @@
 
             Mousetrap.unbind('backspace');
 
+            Mousetrap.unbind(['c', 'C']);
+
             Mousetrap.unbind(['v', 'V']);
 
             Mousetrap.unbind(['f', 'F']);
@@ -922,6 +928,23 @@
 
         toggleMute: function () {
             this.player.muted(!this.player.muted());
+        },
+
+        toggleCrop: function () {
+            var curVideo = $('#video_player_html5_api');
+            if (curVideo[0]) {
+                var multPer = ((curVideo[0].videoWidth / curVideo[0].videoHeight) / (screen.width / screen.height))*100;
+                if (curVideo.css('width') > $('#video_player').css('width')) {
+                    curVideo.css({'width': '100%', 'margin-left': '0'});
+                    this.displayOverlayMsg(i18n.__('Original'));
+                } else if (multPer > 100) {
+                    curVideo.css({'width': multPer + '%', 'margin-left': (100-multPer)/2 + '%'});
+                    this.displayOverlayMsg(i18n.__('Fit screen'));
+                } else if (multPer <= 100) {
+                    this.displayOverlayMsg(i18n.__('Video already fits screen'));
+                }
+                $('.vjs-overlay').css('opacity', '1');
+            }
         },
 
         subtitlesOnOff: function () {
