26f54bb31c249989f997ca57c40def7a3292de91
Daniel Bayley <daniel.bayley@me.com>
2020-11-08 15:54:09 +0000

    Update Casks

--- casks/popcorn-time-beta.rb
+++ casks/popcorn-time-beta.rb
@@ -1,27 +1,28 @@
-cask 'popcorn-time-beta' do
-  version '0.4.3,410'
-  sha256 '842d3eab8b4eca619f922ac3b7a9a699fe94fa33df38a86b312582e787b3c8ae'
+cask "popcorn-time-beta" do
+  version "0.4.3,410"
+  sha256 "842d3eab8b4eca619f922ac3b7a9a699fe94fa33df38a86b312582e787b3c8ae"
 
-  ci = 'https://ci.popcorntime.app/job/Popcorn-Time-Desktop'
+  ci = "https://ci.popcorntime.app/job/Popcorn-Time-Desktop"
   url "#{ci}/lastStableBuild/artifact/build/Popcorn-Time-#{version.before_comma}_osx64.zip"
   appcast ci, configuration: "Last stable build (##{version.after_comma})"
-  name 'Popcorn Time'
-  homepage 'https://popcorntime.app/'
+  name "Popcorn Time"
+  desc "Watch movies and TV shows instantly"
+  homepage "https://popcorntime.app/"
 
   auto_updates true
-  conflicts_with cask: 'popcorn-time'
+  conflicts_with cask: "popcorn-time"
 
-  app 'Popcorn-Time.app'
+  app "Popcorn-Time.app"
 
-  bundle_id = 'com.nw-builder.popcorn-time'
+  bundle_id = "com.nw-builder.popcorn-time"
   uninstall quit: bundle_id
 
   zap trash: [
-               "~/Library/Preferences/#{bundle_id}.plist",
-               '~/Library/Application Support/Popcorn-Time',
-               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
-               '~/Library/Application Support/configstore/popcorn-time.json',
-               "~/Library/Saved Application State/#{bundle_id}.savedState",
-               '~/Library/Caches/Popcorn-Time',
-             ]
+    "~/Library/Preferences/#{bundle_id}.plist",
+    "~/Library/Application Support/Popcorn-Time",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
+    "~/Library/Application Support/configstore/popcorn-time.json",
+    "~/Library/Saved Application State/#{bundle_id}.savedState",
+    "~/Library/Caches/Popcorn-Time",
+  ]
 end
--- casks/popcorn-time.rb
+++ casks/popcorn-time.rb
@@ -1,27 +1,28 @@
-cask 'popcorn-time' do
-  version '0.4.4'
-  sha256 '045dbe37d06e24ed7129dddd922648caaba712dee24685fb3cb1f4782f03ead5'
+cask "popcorn-time" do
+  version "0.4.4"
+  sha256 "045dbe37d06e24ed7129dddd922648caaba712dee24685fb3cb1f4782f03ead5"
 
   url "https://get.popcorntime.app/build/Popcorn-Time-#{version}.pkg"
-  appcast 'https://github.com/popcorn-official/popcorn-desktop/releases.atom'
-  name 'Popcorn Time'
-  homepage 'https://popcorntime.app/'
+  appcast "https://github.com/popcorn-official/popcorn-desktop/releases.atom"
+  name "Popcorn Time"
+  desc "Watch movies and TV shows instantly"
+  homepage "https://popcorntime.app/"
 
   auto_updates true
-  conflicts_with cask: 'popcorn-time-beta'
+  conflicts_with cask: "popcorn-time-beta"
 
   pkg "Popcorn-Time-#{version}.pkg"
 
-  bundle_id = 'com.nw-builder.popcorn-time'
+  bundle_id = "com.nw-builder.popcorn-time"
   uninstall quit:   bundle_id,
             delete: "#{appdir}/Popcorn-Time.app"
 
   zap trash: [
-               "~/Library/Preferences/#{bundle_id}.plist",
-               '~/Library/Application Support/Popcorn-Time',
-               "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
-               '~/Library/Application Support/configstore/popcorn-time.json',
-               "~/Library/Saved Application State/#{bundle_id}.savedState",
-               '~/Library/Caches/Popcorn-Time',
-             ]
+    "~/Library/Preferences/#{bundle_id}.plist",
+    "~/Library/Application Support/Popcorn-Time",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/#{bundle_id}.sfl*",
+    "~/Library/Application Support/configstore/popcorn-time.json",
+    "~/Library/Saved Application State/#{bundle_id}.savedState",
+    "~/Library/Caches/Popcorn-Time",
+  ]
 end
