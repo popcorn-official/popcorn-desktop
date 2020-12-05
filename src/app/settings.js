6f8059e55ca2ec280ab1e9dd98da1af0c2983da3
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2020-12-05 23:03:11 +0200

    Update settings.js

--- src/app/settings.js
+++ src/app/settings.js
@@ -5,8 +5,9 @@ var Settings = {
   projectTwitter: 'popcorntimetv',
   projectTwitter2: 'r_popcorntime',
   projectBlog: 'https://blog.popcorntime.app/',
-  projectForum: 'https://www.reddit.com/r/PopcornTimeApp',
+  projectForum: 'https://www.reddit.com/r/PopcornTime',
   projectForum2: 'https://discuss.popcorntime.app',
+  projectForum3: 'https://www.reddit.com/r/PopcornTimeApp',
 
   statusUrl: 'http://status.popcorntime.app',
   changelogUrl: 'https://github.com/popcorn-official/popcorn-desktop/commits/master',
