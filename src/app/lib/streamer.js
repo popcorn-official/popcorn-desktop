dec12cf327592651faa28009d3f8b6be815215cd
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2021-01-27 08:22:30 +0200

    Update streamer.js

--- src/app/lib/streamer.js
+++ src/app/lib/streamer.js
@@ -34,6 +34,10 @@
     WebTorrentStreamer.prototype = {
 
         initExistTorrents: function() {
+          if (!Settings.continueSeedingOnStart) {
+            return;
+          }
+
           fs.readdir(App.settings.tmpLocation + '/TorrentCache/', (err, files) => {
             if (err) {
               win.error('Read exist torrents failed:', err.name, err.code);
