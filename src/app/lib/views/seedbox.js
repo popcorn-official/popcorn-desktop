54299e0ce5d22643caa3b6e8766f67485c2a5246
Paul Joey Clark <joeytwiddle@gmail.com>
2022-01-24 13:08:01 +0800

    Abbreviate Bytes to B, to reduce resizing
    "B" has a more consistent width with "KB" and "MB"

--- src/app/lib/views/seedbox.js
+++ src/app/lib/views/seedbox.js
@@ -13,12 +13,12 @@
 
 	var formatBytes = function (bytes, decimals) {
 		if (bytes === 0) {
-			return '0 Bytes';
+			return '0 B';
 		}
 
 		let k = 1024,
 			dm = decimals || 2,
-			sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+			sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
 			i = Math.floor(Math.log(bytes) / Math.log(k));
 		return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 	};
