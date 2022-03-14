8ac9175fadac795ee98d3716391f038b36fee909
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2022-03-14 19:24:46 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -277,7 +277,7 @@ gulp.task('compresszip', () => {
         return gulp
           .src(sources + '/**')
           .pipe(
-            zip(pkJson.name + '-' + curVersion() + '_' + platform + '.zip')
+            zip(pkJson.name + '-' + curVersion() + '-' + platform + '.zip')
           )
           .pipe(gulp.dest(releasesDir))
           .on('end', () => {
@@ -307,7 +307,7 @@ gulp.task('compressUpdater', () => {
         console.log('Packaging updater for: %s', platform);
         return gulp
           .src(path.join('build', updateFile))
-          .pipe(zip('update-' + curVersion() + '_' + platform + '.zip'))
+          .pipe(zip('update-' + curVersion() + '-' + platform + '.zip'))
           .pipe(gulp.dest(releasesDir))
           .on('end', () => {
             console.log(
@@ -667,7 +667,7 @@ gulp.task('prepareUpdater:win', () => {
           )
           .pipe(gulpRename('update.exe'))
           .pipe(gulp.dest(path.join(process.cwd(), releasesDir)))
-          .pipe(zip('update-' + curVersion() + '_' + platform + '.zip'))
+          .pipe(zip('update-' + curVersion() + '-' + platform + '.zip'))
           .pipe(gulp.dest(releasesDir))
           .on('end', () => {
             console.log(
