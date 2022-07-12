6d109b7cd10855a34481c26cbcd87231ceceef67
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2022-07-12 04:40:13 +0300

    Alternative 'more..' API for when main doesn't have support (#2556)

--- src/app/lib/views/movie_detail.js
+++ src/app/lib/views/movie_detail.js
@@ -82,10 +82,11 @@
         return;
       }
       const provider = App.Config.getProviderForType('movie')[0];
+      const altShowAll = provider.config.noShowAll ? _.shuffle((Settings.dhtInfo.server ? Settings.dhtInfo.server.split(',') : Settings.customServers.movie).filter(a => !a.includes(provider.apiURL))) : null;
       const torrentList = new App.View.TorrentList({
         model: new Backbone.Model({
           provider,
-          promise: provider.torrents(this.model.get('imdb_id'), lang),
+          promise: provider.torrents(this.model.get('imdb_id'), lang, altShowAll),
         }),
       });
       this.getRegion('TorrentList').show(torrentList);
