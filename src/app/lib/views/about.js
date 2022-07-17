ae9fd6c47a05adf2f856e5f879649af43b788d2a
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2022-07-17 17:23:38 +0300

    Update about.styl

--- src/app/lib/views/about.js
+++ src/app/lib/views/about.js
@@ -25,10 +25,6 @@
 
         onBeforeDestroy: function () {
             Mousetrap.unbind(['esc', 'backspace']);
-            if (!App.ViewStack.includes('settings-container-contain')) {
-                $('.filter-bar').show();
-                $('#header').removeClass('header-shadow');
-            }
             $('#movie-detail').show();
         },
 
