211463c327e7b1d8f9f695dfd1ca103621bb3ba7
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2022-08-21 23:57:22 +0300

    Update help.js

--- src/app/lib/views/help.js
+++ src/app/lib/views/help.js
@@ -17,7 +17,7 @@
                 i18n.__('You can paste magnet links anywhere in %s with CTRL+V.', Settings.projectName),
                 i18n.__('You can drag & drop a .torrent file into %s.', Settings.projectName),
                 i18n.__('The %s project was forked from the Popcorn Time project that started in February 2014 and has already had 150 people that contributed more than 3000 times to it\'s development in August 2014.', Settings.projectName),
-                i18n.__('If a subtitle for a TV series is missing, you can add it on %s. And the same way for a movie, but on %s.', 'opensubtitles.org', 'yifysubtitles.com'),
+                i18n.__('If a subtitle for a movie or an episode is missing, you can add it on %s.', 'opensubtitles.org'),
                 i18n.__('If you\'re experiencing connection drop issues, try to reduce the DHT Limit in settings.'),
                 i18n.__('If you search \"1998\", you can see all the movies or TV series that came out that year.'),
                 i18n.__('You can login to Trakt.tv to save all your watched items, and synchronize them across multiple devices.'),
