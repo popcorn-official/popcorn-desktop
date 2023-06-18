3323c5705adfb5f03854eb7e1201734999c6b4b9
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2023-06-18 03:08:10 +0300

    Update show_detail.js

--- src/app/lib/views/show_detail.js
+++ src/app/lib/views/show_detail.js
@@ -263,7 +263,7 @@
         },
         localizeTexts: function () {
             const locale = this.model.get('locale');
-            let title = App.currentview !== 'anime' ? this.model.get('title') : this.model.get('title1');
+            let title = this.model.get('title');
             if (Settings.translateTitle === 'translated-origin' || Settings.translateTitle === 'translated') {
                 if (locale && locale.title) {
                     title = locale.title;
