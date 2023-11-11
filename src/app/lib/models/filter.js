d609c88167c3f2410343cd47ad76781400501f5f
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2023-11-11 19:28:19 +0200

    Added selector to filter watched movies instead of bookmarked ones in the Favorites tab (#2785)

--- src/app/lib/models/filter.js
+++ src/app/lib/models/filter.js
@@ -7,6 +7,7 @@
             this.set('load', false);
             this.set('genres', []);
             this.set('sorters', []);
+            this.set('kinds', []);
             this.set('types', []);
             this.set('ratings', []);
             this.init();
@@ -14,6 +15,7 @@
             this.get('provider').filters().then((filters) => {
                 this.set('genres', filters.genres || []);
                 this.set('sorters', filters.sorters || []);
+                this.set('kinds', filters.kinds || []);
                 this.set('types', filters.types || []);
                 this.set('ratings', filters.ratings || []);
 
@@ -26,6 +28,7 @@
         init() {
             this.set('sorter', this.get('sorter') || Object.keys(this.get('sorters'))[0]);
             this.set('genre', this.get('genre') || Object.keys(this.get('genres'))[0]);
+            this.set('kind', this.get('kind') || Object.keys(this.get('kinds'))[0]);
             this.set('type', this.get('type') || Object.keys(this.get('types'))[0]);
             this.set('order', this.get('order') || -1);
             this.set('rating', this.get('rating') || Object.keys(this.get('ratings'))[0]);
