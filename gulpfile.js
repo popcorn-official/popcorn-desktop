3aa1216d574b5e337e597530aacce7b09dd92bfb
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2024-01-05 01:25:15 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 /********
  * setup *
  ********/
-const nwVersion = '0.82.0',
+const nwVersion = '0.44.5',
   availablePlatforms = ['linux32', 'linux64', 'win32', 'win64', 'osx64'],
   releasesDir = 'build',
   nwFlavor = 'sdk';
