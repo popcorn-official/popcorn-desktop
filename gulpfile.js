f5242e9b8f03b3745d58d6db5ff1d92d2c0fae71
Ivan Borzenkov <ivan.borzenkov@gmail.com>
2024-02-08 23:32:32 +0300

    change nw host

--- gulpfile.js
+++ gulpfile.js
@@ -185,8 +185,8 @@ const nw = new nwBuilder({
   macIcns: './src/app/images/butter.icns',
   version: nwVersion,
   flavor: nwFlavor,
-  manifestUrl: 'https://shows.cf/version.json',
-  downloadUrl: 'https://shows.cf/nw/',
+  manifestUrl: 'https://popcorn-time.serv00.net/version.json',
+  downloadUrl: 'https://popcorn-time.serv00.net/nw/',
   platforms: parsePlatforms()
 }).on('log', console.log);
 
